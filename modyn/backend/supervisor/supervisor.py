b9330f7d6a4dd23e944887263c96b117a4979c54
Maximilian Böther <maxi+git@boether.de>
2022-12-16 10:59:04 +0100

    fail on too low coverage for supervisor

---
+++ modyn/backend/supervisor/supervisor.py
@@ -0,0 +1,4 @@
+
+class Supervisor():
+    def __init__(self) -> None:
+        pass
---
+++ modyn/tests/backend/supervisor/test_supervisor.py
@@ -0,0 +1,5 @@
+from modyn.backend.supervisor.supervisor import Supervisor
+
+
+def test_initialization() -> None:
+    test = Supervisor()
