8e0158b4e048d2a92b1399f0fdd2b335341ff4bc
Maximilian Böther <maxi+git@boether.de>
2022-12-19 13:05:09 +0100

    post meeting pre lunch commit

--- modyn/backend/supervisor/supervisor.py
+++ modyn/backend/supervisor/supervisor.py
@@ -23,12 +23,12 @@ class Supervisor():
 
         if replay_at is None:
             self.experiment_mode = False
-            self.setup_data_pulling()
         else:
             self.experiment_mode = True
             self.replay_at = replay_at
 
     def validate_pipeline_config_schema(self, pipeline_config: dict) -> bool:
+        # TODO(MaxiBoether): Actually write the schema.
         schema_path = pathlib.Path(os.path.abspath(__file__)).parent.parent.parent / "config" / "pipeline-schema.yaml"
         assert schema_path.is_file(), "Did not find pipeline configuration schema."
         with open(schema_path, "r") as f:
@@ -49,21 +49,41 @@ class Supervisor():
             logger.error(f"Model {model_id} is not available within Modyn.")
             return False
 
-        # TODO: More checks.
+        # TODO(MaxiBoether): More checks.
 
         return True
 
     def validate_pipeline_config(self, pipeline_config: dict) -> bool:
         return self.validate_pipeline_config_schema(pipeline_config) and self.validate_pipeline_config_content(pipeline_config)
 
-    def validate_system(self, modyn_config: dict) -> bool:
+    def dataset_available(self, modyn_config: dict) -> bool:
         return True
 
-    def setup_data_pulling(self) -> None:
+    def trainer_available(self, modyn_config: dict) -> bool:
+        return True
+
+    def validate_system(self, modyn_config: dict) -> bool:
+        return self.dataset_available(modyn_config) and self.trainer_available(modyn_config)
+
+    def wait_for_new_data(self) -> None:
         pass
 
     def initial_pass(self) -> None:
         pass
 
+    def replay_data(self) -> None:
+        pass
+
+    def end_pipeline(self) -> None:
+        # deregister etc
+        pass
+
     def pipeline(self) -> None:
         self.initial_pass()
+        if self.experiment_mode:
+            self.replay_data()
+        else:
+            # TODO(MaxiBoether): think about data coming in between initial pass and pulling. probably just pass timestamp before initial pass started to pull and then get notified about that data
+            self.wait_for_new_data()
+        
+        self.end_pipeline()
