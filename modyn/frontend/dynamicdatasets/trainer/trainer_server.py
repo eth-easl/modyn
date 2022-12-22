83020c079cff55aa8d335335d0b23866f41ac6b9
fotstrt <fotstrt95@gmail.com>
2022-12-22 20:28:51 +0200

    enable start of training and add examples to test it

--- modyn/frontend/dynamicdatasets/trainer/trainer_server.py
+++ modyn/frontend/dynamicdatasets/trainer/trainer_server.py
@@ -4,6 +4,8 @@ import sys
 from concurrent import futures
 from pathlib import Path
 import logging
+import torch
+import multiprocessing as mp
 
 import yaml
 
@@ -14,6 +16,10 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 from modyn.frontend.dynamicdatasets.trainer.trainer_server_pb2_grpc import TrainerServerServicer, add_TrainerServerServicer_to_server
 from modyn.frontend.dynamicdatasets.trainer.trainer_server_pb2 import TrainerServerRequest, TrainerServerResponse
 
+# TODO(fotstrt): replace with dynamic loading
+from modyn.frontend.dynamicdatasets.trainer.data.cifar_dataset import get_cifar_datasets
+from modyn.frontend.dynamicdatasets.trainer.models.resnet import ResNet
+
 logging.basicConfig(format='%(asctime)s %(message)s')
 
 class TrainerGRPCServer:
@@ -22,10 +28,48 @@ class TrainerGRPCServer:
     def __init__(self):
         pass
 
+    def prepare_dataloaders(self, dataset_id, num_dataloaders, batch_size):
+
+        if dataset_id == "cifar10":
+            train_set, val_set = get_cifar_datasets()
+
+            train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+
+            val_dataloader = torch.utils.data.DataLoader(val_set, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
+            return train_dataloader, val_dataloader
+        else:
+            return None, None
+
+
     def start_training(self, request: TrainerServerRequest, context: grpc.ServicerContext) -> TrainerServerResponse:
 
-        data_info = request.data_info
-        print(data_info.dataset_id, data_info.num_dataloaders)
+        # TODO(fotstrt): generalize
+        train_dataloader, val_dataloader = self.prepare_dataloaders(
+            request.data_info.dataset_id,
+            request.data_info.num_dataloaders,
+            request.hyperparameters.batch_size
+        )
+
+
+        # this is tailor-made for a specific example
+        # TODO(fotstrt): generalize
+        model = ResNet(
+            'resnet18',
+            'SGD',
+            {'lr': 0.1},
+            torch.nn.CrossEntropyLoss,
+            10,
+            train_dataloader,
+            val_dataloader,
+            0
+        )
+
+        p = mp.Process(target=model.train)
+        p.start()
+        p.join()
 
         return TrainerServerResponse(training_id=10)
 
@@ -43,6 +87,9 @@ def serve(config: dict) -> None:
 
 
 if __name__ == '__main__':
+
+    mp.set_start_method('spawn')
+
     logging.basicConfig(level=logging.INFO)
     if len(sys.argv) != 2:
         print("Usage: python trainer_server.py <config_file>")
