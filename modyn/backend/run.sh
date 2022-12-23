213ea70db6a0b2eb667fc01391dc981d026ab235
Maximilian Böther <2116466+MaxiBoether@users.noreply.github.com>
2022-12-23 09:32:39 +0100

    Update modyn/backend/run.sh
    Co-authored-by: Viktor Gsteiger <vgsteiger@student.ethz.ch>

--- modyn/backend/run.sh
+++ modyn/backend/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-exec python ./modyn/backend/newqueue/newqueue_server.py ./modyn/config/config.yaml &
 exec python ./modyn/backend/odm/odm_server.py ./modyn/config/config.yaml &
 exec python ./modyn/backend/ptmp/ptmp_server.py ./modyn/config/config.yaml &
 exec python ./modyn/backend/selector/selector_server.py ./modyn/config/config.yaml &
