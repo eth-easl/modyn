5e4b1605a755fe492989f62110e5e259719ad820
Maximilian Böther <maxi+git@boether.de>
2023-01-06 15:29:32 +0100

    seriously pep

---
+++ integrationtests/storage/test_storage.py
@@ -0,0 +1,7 @@
+
+def main() -> None:
+    return True
+
+
+if __name__ == '__main__':
+    main()
--- integrationtests/test_docker_compose.py
+++ integrationtests/test_docker_compose.py
@@ -7,8 +7,9 @@ import psycopg2
 
 SCRIPT_PATH = pathlib.Path(os.path.realpath(__file__))
 
-TIMEOUT = 10 # seconds
+TIMEOUT = 10  # seconds
 CONFIG_FILE = SCRIPT_PATH.parent.parent / "modyn" / "config" / "config.yaml"
+
 
 def terminate_on_timeout(start_time: int) -> None:
     curr_time = round(time.time())
@@ -18,21 +19,23 @@ def terminate_on_timeout(start_time: int) -> None:
 
     raise TimeoutError("Reached timeout")
 
+
 def get_modyn_config() -> dict:
     with open(CONFIG_FILE, "r", encoding="utf-8") as config_file:
         config = yaml.safe_load(config_file)
 
     return config
 
+
 def storage_running() -> bool:
     return True
 
-def storage_db_running() -> bool:
-    config = get_modyn_config()
 
-    conn = None
+def storage_db_running() -> bool:
+    # config = get_modyn_config()
+    # conn = None
     try:
-        #psycopg2.connect(
+        # psycopg2.connect(
         #                host=config['odm']['postgresql']['host'],
         #                port=config['odm']['postgresql']['port'],
         #                database=config['odm']['postgresql']['database'],
@@ -53,8 +56,10 @@ def selector_running() -> bool:
     # TODO(MaxiBoether): implement this when storage is merged and docker entrypoint works
     return True
 
+
 def system_running() -> bool:
     return storage_db_running() and storage_running() and selector_running()
+
 
 def main() -> None:
     start_time = round(time.time())
@@ -65,6 +70,7 @@ def main() -> None:
 
         terminate_on_timeout(start_time)
         time.sleep(1)
+
 
 if __name__ == '__main__':
     main()
