e8dc1c9e0b96cd09013909966d113115373c42ae
vgsteiger <vgsteiger@student.ethz.ch>
2023-01-09 19:19:33 +0100

    Fix deprecation warning

--- modyn/storage/internal/database/base.py
+++ modyn/storage/internal/database/base.py
@@ -1,6 +1,7 @@
 """Base model."""
 
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
