490a02b2579b75360e1b5ab2a0d7c973157dfad1
Maximilian Böther <maxi+git@boether.de>
2023-01-10 16:28:34 +0100

    fix filename

--- integrationtests/run.sh
+++ integrationtests/run.sh
@@ -7,4 +7,4 @@ echo "Integration tests are located in $SCRIPT_DIR"
 echo "Running basic availability tests"
 python $SCRIPT_DIR/test_docker_compose.py
 echo "Running storage integration tests"
-python $SCRIPT_DIR/storage/test_storage.py
+python $SCRIPT_DIR/storage/integrationtest_storage.py
