8f75f041b0d421d1f3ba88de118f896e395b3e3e
fotstrt <fotstrt95@gmail.com>
2023-01-13 23:16:35 +0100

    add missing import

--- modyn/models/__init__.py
+++ modyn/models/__init__.py
@@ -3,6 +3,8 @@
 """
 import os
 
+from .resnet18.resnet18 import ResNet18  # noqa: F401
+
 files = os.listdir(os.path.dirname(__file__))
 files.remove("__init__.py")
 __all__ = [f[:-3] for f in files if f.endswith(".py")]
