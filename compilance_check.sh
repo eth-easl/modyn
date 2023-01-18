36346605ee3c9cd1a2f1576a951bf1a9a0201e19
vgsteiger <vgsteiger@student.ethz.ch>
2023-01-18 14:00:10 +0100

    Created automatic compilance script for easier dev

---
+++ compilance_check.sh
@@ -0,0 +1,54 @@
+#!/bin/zsh
+echo "Running compilance check"
+
+echo "Running auto-formatters"
+
+source ~/.zshrc
+
+eval "$(conda shell.zsh hook)"
+conda activate modyn > /dev/null
+
+isort . > /dev/null
+autopep8 modyn --recursive --in-place --pep8-passes 2000 > /dev/null
+black modyn --verbose --config black.toml > /dev/null
+
+echo "Running linters"
+
+if flake8 ; then
+    echo "No flake8 errors"
+else
+    echo "flake8 errors"
+    exit 1
+fi
+
+if isort . --check --diff ; then
+    echo "No isort errors"
+else
+    echo "isort errors"
+    exit 1
+fi
+
+if black --check modyn --config black.toml ; then
+    echo "No black errors"
+else
+    echo "black errors"
+    exit 1
+fi
+
+if pylint modyn ; then
+    echo "No pylint errors"
+else
+    echo "pylint errors"
+    exit 1
+fi
+
+echo "Running tests"
+
+if pytest ; then
+    echo "No pytest errors"
+else
+    echo "pytest errors"
+    exit 1
+fi
+
+echo "Successfull compilance check"
