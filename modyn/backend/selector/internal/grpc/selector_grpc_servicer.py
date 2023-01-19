091b967d332c6144b69094b4d814f250c6853320
Kevin Shao <knshao@gmail.com>
2023-01-19 14:15:47 +0100

    Unify SelectionStrategy return type to floating point weights (#94)
    This is a small PR that addresses the previous issue that the various
    selectors had slightly different interfaces. Now, they all return a List
    of tuples(key, score).
    
    Co-authored-by: Kevin Shao <kshao23@mit.edu>
    Co-authored-by: Maximilian Böther <2116466+MaxiBoether@users.noreply.github.com>

--- modyn/backend/selector/internal/grpc/selector_grpc_servicer.py
+++ modyn/backend/selector/internal/grpc/selector_grpc_servicer.py
@@ -26,12 +26,13 @@ class SelectorGRPCServicer(SelectorServicer):
         training_id = self.selector.register_training(request.training_set_size, request.num_workers)
         return TrainingResponse(training_id=training_id)
 
-    def get_sample_keys_and_metadata(  # pylint: disable-next=unused-argument
+    def get_sample_keys_and_weight(  # pylint: disable-next=unused-argument
         self, request: GetSamplesRequest, context: grpc.ServicerContext
     ) -> SamplesResponse:
         logger.info(f"Fetching samples for request - {str(request)}")
-        samples_keys = self.selector.get_sample_keys_and_metadata(
+        samples = self.selector.get_sample_keys_and_weight(
             request.training_id, request.training_set_number, request.worker_id
         )
-        samples_keys = [sample[0] for sample in samples_keys]
-        return SamplesResponse(training_samples_subset=samples_keys)
+        samples_keys = [sample[0] for sample in samples]
+        samples_weights = [sample[1] for sample in samples]
+        return SamplesResponse(training_samples_subset=samples_keys, training_samples_weight=samples_weights)
