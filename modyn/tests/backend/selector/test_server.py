26791e1b2d6d0037bb501663ee30a8a6739994b1
Kevin Shao <kshao23@mit.edu>
2023-01-21 10:57:30 +0100

    Fix typos

--- modyn/tests/backend/selector/test_server.py
+++ modyn/tests/backend/selector/test_server.py
@@ -38,16 +38,16 @@ def setup():
     with MetadataDatabaseConnection(get_minimal_modyn_config()) as database:
         database.create_tables()
 
-        trainig = Training(1)
-        database.session.add(trainig)
+        training = Training(1)
+        database.session.add(training)
         database.session.commit()
 
-        metadata = Metadata("test_key", 0.5, False, 1, b"test_data", trainig.training_id)
+        metadata = Metadata("test_key", 0.5, False, 1, b"test_data", training.training_id)
 
-        metadata.metadata_id = 1  # SQLite does not support autoincrement for composite primary keys #pylint
+        metadata.metadata_id = 1  # SQLite does not support autoincrement for composite primary keys 
         database.session.add(metadata)
 
-        metadata2 = Metadata("test_key2", 0.75, True, 2, b"test_data2", trainig.training_id)
+        metadata2 = Metadata("test_key2", 0.75, True, 2, b"test_data2", training.training_id)
 
         metadata2.metadata_id = 2  # SQLite does not support autoincrement for composite primary key
         database.session.add(metadata2)
