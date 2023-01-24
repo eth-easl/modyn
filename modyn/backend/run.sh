3737d384b231ce4337e48589afb125cdc68175df
Roxana Stiuca <roxana9stiuca@gmail.com>
2023-01-24 14:44:22 +0100

    add MetadataProcessor to Docker setup (missing: depends on metadata-database)

--- modyn/backend/run.sh
+++ modyn/backend/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-exec python ./modyn/backend/metadata_processor/metadata_processor.py ./modyn/config/config.yaml &
 exec python ./modyn/backend/metadata_database/metadata_database_server.py ./modyn/config/config.yaml &
 exec python ./modyn/backend/selector/selector_server.py ./modyn/config/config.yaml &
