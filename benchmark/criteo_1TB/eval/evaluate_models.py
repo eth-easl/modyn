05a7edf98bf6ee4b9d4a47034a0eb7a9ebe51a4b
Maximilian Böther <maxi+git@boether.de>
2023-02-22 11:59:46 +0100

    work

--- benchmark/criteo_1TB/eval/evaluate_models.py
+++ benchmark/criteo_1TB/eval/evaluate_models.py
@@ -2,10 +2,10 @@ import argparse
 import logging
 import os
 import pathlib
-import random
-import shutil
-import time
+import sys
 import json
+
+from evaluation_support import evaluate_model
 
 logging.basicConfig(
     level=logging.NOTSET,
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 PIPELINES = {"finetune_noreset": "Finetune",
             } # TODO(MaxiBoether): Add our pipelines after defining them
-RESULTS = {}
+NUM_DAYS = 22
 
 def setup_argparser() -> argparse.ArgumentParser:
     parser_ = argparse.ArgumentParser(description="Criteo Model Evaluation Script")
@@ -26,6 +26,9 @@ def setup_argparser() -> argparse.ArgumentParser:
     parser_.add_argument(
         "output", type=pathlib.Path, action="store", help="Path to output json file"
     )
+    parser_.add_argument(
+        "evaluation_data", type=pathlib.Path, action="store", help="Path to evaluation data (day 23)"
+    )
 
     return parser_
 
@@ -33,35 +36,69 @@ def validate_output_file_does_not_exist(output: pathlib.Path):
     if output.exists():
         raise ValueError(f"Output file {output} already exists.")
 
-def validate_model_directory(dir: pathlib.Path):
+def validate_model_directory(dir: pathlib.Path) -> dict:
+    pipelines_to_evaluate = {}
+
     for pipeline in PIPELINES:
         pipeline_dir = dir / pipeline.replace(' ', '_')
-        models = list(filter(os.path.isdir, os.listdir(pipeline_dir)))
-        if len(models) == 0:
-            logger.warning(f"Pipeline {pipeline}: No trained model found, ignoring.")
+
+        if not pipeline_dir.exists():
+            logger.warning(f"Did not find pipeline {pipeline}, ignoring.")
             continue
 
-        if any((not name.isdigit() for name in models)):
-            raise ValueError(f"Found invalid (non-numeric) model for pipeline {pipeline}, don't know how to proceed")
+        runs = list(filter(os.path.isdir, os.listdir(pipeline_dir)))
+        if len(runs) == 0:
+            logger.warning(f"Pipeline {pipeline}: No runs found, ignoring.")
+            continue
 
-        models.sort(key = int)
-        latest_model_path
+        if any((not name.isdigit() for name in runs)):
+            raise ValueError(f"Found invalid (non-numeric) run for pipeline {pipeline}, don't know how to proceed")
 
+        runs.sort(key = int)
+        latest_run_path = pipeline_dir / runs[-1]
+        
+        for i in range(0, NUM_DAYS):
+            model_path = latest_run_path / f"{i}.modyn"
+            if not model_path.exists():
+                raise ValueError(f"Pipeline {pipeline}: Run {runs[-1]} is invalid, could not find model for day {i}.")
 
-def evaluate(dir: pathlib.Path):
-    pass
+        pipelines_to_evaluate[pipeline] = latest_run_path
 
-def write_results_to_file(output: pathlib.Path):
-    pass
+    return pipelines_to_evaluate
 
+def evaluate_pipeline(dir: pathlib.Path, evaluation_data: pathlib.Path) -> dict:
+    pipeline_data = {}
+    for i in range(0, NUM_DAYS):
+        model_path = dir / f"{i}.modyn"
+        pipeline_data[i] = evaluate_model(model_path, evaluation_data)
 
+    return pipeline_data
+
+def evaluate(pipelines_to_evaluate: dict, evaluation_data: pathlib.Path) -> dict:
+    results = {}
+    for pipeline, path in pipelines_to_evaluate:
+        pipeline_data = evaluate_pipeline(path, evaluation_data)
+        results[pipeline] = pipeline_data
+
+    return results
+
+def write_results_to_file(results: dict, output: pathlib.Path):
+    with open(output, "w") as output_file:
+        json.dump(results, output_file)
 
 def main():
     parser = setup_argparser()
     args = parser.parse_args()
     validate_output_file_does_not_exist(args.output)
-    validate_model_directory(args.dir)
-    evaluate(args.dir)
+    pipelines_to_evaluate = validate_model_directory(args.dir)
+
+    if len(pipelines_to_evaluate) == 0:
+        logger.error("Found no pipeline data to evaluate, exiting.")
+        sys.exit(-1)
+
+    results = evaluate(pipelines_to_evaluate, args.evaluation_data)
+
+    write_results_to_file(results, args.output)
 
 if __name__ == "__main__":
     main()
---
+++ benchmark/criteo_1TB/eval/evaluation_support.py
@@ -0,0 +1,6 @@
+import pathlib
+import torch
+
+
+def evaluate_model(model_path: pathlib.Path, evaluation_data: pathlib.Path):
+    pass
