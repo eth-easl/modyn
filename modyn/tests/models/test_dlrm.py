e31b9390e754449030b75b6e667fc3a5692306f2
fotstrt <fotstrt95@gmail.com>
2023-02-27 10:41:47 +0100

    change dlrm dimensions at test

---
+++ modyn/tests/models/test_dlrm.py
@@ -0,0 +1,61 @@
+import torch
+from modyn.models.dlrm.dlrm import DLRM
+
+
+def test_dlrm_init():
+    model_configuration = {
+        "embedding_dim": 16,
+        "interaction_op": "dot",
+        "hash_indices": False,
+        "bottom_mlp_sizes": [64, 32, 16],
+        "top_mlp_sizes": [64, 64, 32, 16, 1],
+        "embedding_type": "multi_table",
+        "use_cpp_mlp": False,
+        "amp": False,
+        "device": "cpu",
+        "num_numerical_features": 13,
+        "categorical_features_info": {
+            "cat_0": 7912889,
+            "cat_1": 33823,
+            "cat_2": 17139,
+            "cat_3": 7339,
+            "cat_4": 20046,
+            "cat_5": 4,
+            "cat_6": 7105,
+            "cat_7": 1382,
+            "cat_8": 63,
+            "cat_9": 5554114,
+            "cat_10": 582469,
+            "cat_11": 245828,
+            "cat_12": 11,
+            "cat_13": 2209,
+            "cat_14": 10667,
+            "cat_15": 104,
+            "cat_16": 4,
+            "cat_17": 968,
+            "cat_18": 15,
+            "cat_19": 8165896,
+            "cat_20": 2675940,
+            "cat_21": 7156453,
+            "cat_22": 302516,
+            "cat_23": 12022,
+            "cat_24": 97,
+            "cat_25": 35,
+        },
+    }
+    model = DLRM(model_configuration)
+    order_list = [19, 0, 21, 9, 20, 10, 22, 11, 1, 4, 2, 23, 14, 3, 6, 13, 7, 17, 15, 24, 8, 25, 18, 12, 5, 16]
+
+    assert torch.equal(model.model._embedding_ordering, torch.tensor(order_list))
+    data = {
+        "numerical_input": torch.ones((64, 13), dtype=torch.float32),
+        "categorical_input": torch.ones((64, 26), dtype=torch.long),
+    }
+    model.model(data)
+
+    test_data = torch.tensor(list(range(26, 52)), dtype=torch.long).expand(64, -1)
+    input_data = torch.tensor([x + 26 for x in order_list], dtype=torch.long).expand(64, -1)
+    reordered_test_data = model.model.reorder_categorical_input(test_data)
+    assert reordered_test_data.shape == (64, 26)
+    assert reordered_test_data.dtype == torch.long
+    assert torch.equal(reordered_test_data, input_data)
