83a9d26577c434da4fb0731eaa4dcc3d1949ff1b
Viktor Gsteiger <vgsteiger@student.ethz.ch>
2023-03-14 15:10:33 +0100

    Update modyn/backend/selector/internal/trigger_sample/__init__.py
    Co-authored-by: Maximilian Böther <2116466+MaxiBoether@users.noreply.github.com>

--- modyn/backend/selector/internal/trigger_sample/__init__.py
+++ modyn/backend/selector/internal/trigger_sample/__init__.py
@@ -1,5 +1,5 @@
 """
-This submodule provides the base functionality for the selector server component.
+This submodule implements necessary functions to persist a calculated trigger training set to disk.
 """
 import os
 
