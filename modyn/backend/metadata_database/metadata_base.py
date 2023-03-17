b3f2a5a894acbac84606772e1f5fb47edc626a19
Maximilian Böther <maxi+git@boether.de>
2023-03-17 16:47:21 +0100

    start working on partitioning

--- modyn/backend/metadata_database/metadata_base.py
+++ modyn/backend/metadata_database/metadata_base.py
@@ -2,6 +2,67 @@
 
 from sqlalchemy.orm import DeclarativeBase
 
+from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.sql.ddl import DDL
+from sqlalchemy import event
+
+# Kudos: https://stackoverflow.com/questions/61545680/postgresql-partition-and-sqlalchemy
+
+class PartitionByMeta(DeclarativeMeta):
+    def __new__(cls, clsname, bases, attrs, *, partition_by, partition_type):
+
+        @classmethod
+        def get_partition_name(cls_, suffix):
+            return f'{cls_.__tablename__}_{suffix}'
+
+        @classmethod
+        def create_partition(cls_, suffix, partition_stmt, subpartition_by=None, subpartition_type=None, unlogged=True):
+            if suffix not in cls_.partitions: # TODO: what happens on restart? how do we handle existing partitions?
+
+                attrs = {'__tablename__': cls_.get_partition_name(suffix)}
+                if unlogged:
+                    attrs['__table_args__'] = ({"prefixes": ["UNLOGGED"]})
+                
+                partition = PartitionByMeta(
+                    f'{clsname}{suffix}',
+                    bases,
+                    attrs,
+                    partition_type = subpartition_type,
+                    partition_by=subpartition_by,
+                )
+
+                partition.__table__.add_is_dependent_on(cls_.__table__)
+
+                event.listen(
+                    partition.__table__,
+                    'after_create',
+                    DDL(
+                        f"""
+                        ALTER TABLE {cls_.__tablename__}
+                        ATTACH PARTITION {partition.__tablename__}
+                        {partition_stmt};
+                        """
+                    )
+                )
+                
+                cls_.partitions[suffix] = partition
+            
+            return cls_.partitions[suffix]
+        
+        if partition_by is not None:
+            attrs.update(
+                {
+                    '__table_args__': attrs.get('__table_args__', ())
+                    + (dict(postgresql_partition_by=f'{partition_type.upper()}({partition_by})'),),
+                    'partitions': {},
+                    'partitioned_by': partition_by,
+                    'get_partition_name': get_partition_name,
+                    'create_partition': create_partition
+                }
+            )
+        
+        return super().__new__(cls, clsname, bases, attrs)
+    
 
 class MetadataBase(DeclarativeBase):
     pass
--- modyn/backend/metadata_database/models/selector_state_metadata.py
+++ modyn/backend/metadata_database/models/selector_state_metadata.py
@@ -1,13 +1,26 @@
 """SelectorStateMetadata model."""
 
-from modyn.backend.metadata_database.metadata_base import MetadataBase
-from sqlalchemy import BigInteger, Boolean, Column, ForeignKey, Index, Integer
+from modyn.backend.metadata_database.metadata_base import PartitionByMeta, MetadataBase
+from sqlalchemy import BigInteger, Boolean, Column, Integer
 from sqlalchemy.dialects import sqlite
+
+from typing import Any
 
 BIGINT = BigInteger().with_variant(sqlite.INTEGER(), "sqlite")
 
+class SelectorStateMetadataMixin:
+    pipeline_id = Column("pipeline_id", Integer, primary_key=True)
+    sample_key = Column("sample_key", BIGINT, primary_key=True)
+    seen_in_trigger_id = Column("seen_in_trigger_id", Integer)
+    used = Column("used", Boolean, default=False)
+    timestamp = Column("timestamp", BigInteger)
+    label = Column("label", Integer)
 
-class SelectorStateMetadata(MetadataBase):
+
+# 1. Partition level: pipeline_id (LIST, manually create partition on new pipeline)
+# 2. Parititon level: seen_in_trigger_id (LIST, manually create partition on trigger for next trigger)
+# 3. Partition level: sample_key (HASH, MOD 16/32/64)
+class SelectorStateMetadata(SelectorStateMetadataMixin, MetadataBase, metaclass=PartitionByMeta, partition_by="pipeline_id", partition_type="LIST"): # TODO: do not partition for sqllite
     """SelectorStateMetadata model.
 
     Metadata persistently stored by the Selector.
@@ -16,13 +29,29 @@ class SelectorStateMetadata(MetadataBase):
     """
 
     __tablename__ = "selector_state_metadata"
-    # See https://docs.sqlalchemy.org/en/13/core/metadata.html?highlight=extend_existing#sqlalchemy.schema.Table.params.extend_existing  # noqa: E501
-    selector_state_metadata_id = Column("selector_state_metadata_id", BIGINT, autoincrement=True, primary_key=True)
-    pipeline_id = Column("pipeline_id", Integer, ForeignKey("pipelines.pipeline_id"), nullable=False, index=True)
-    sample_key = Column("sample_key", BIGINT, nullable=False)
-    seen_in_trigger_id = Column("seen_in_trigger_id", Integer)
-    used = Column("used", Boolean, default=False)
-    timestamp = Column("timestamp", BigInteger)
-    label = Column("label", Integer)
-    # ssm_pipeline_seen_idx: Optimizes new data strategy
-    __table_args__ = (Index("ssm_pipeline_seen_idx", "pipeline_id", "seen_in_trigger_id"), {"extend_existing": True})
+
+    # ssm_pipeline_seen_idx: Optimizes new data strategy (Index("ssm_pipeline_seen_idx", "pipeline_id", "seen_in_trigger_id") TODO: integrate index again on the fly
+    __table_args__ = ({"prefixes": ["UNLOGGED"]})
+
+    @staticmethod
+    def add_pipeline(pipeline_id: int) -> Any: # todo investigate type
+        partition_stmt = f"FOR VALUES IN ({pipeline_id})"
+        partition_suffix = f"_pid{pipeline_id}"
+        return SelectorStateMetadata.create_partition(partition_suffix, partition_stmt=partition_stmt, subpartition_by="seen_in_trigger_id", subpartition_type="LIST")
+
+    @staticmethod
+    def add_trigger(pipeline_id: int, trigger_id: int) -> None:
+        # TODO: this is not the best/correct way to obtain the partition. we should somehow implement a "get partition" (and populate all partitions on boot and let add_pipeline fail if partition already exists!)
+        pipeline_partition = SelectorStateMetadata.add_pipeline(pipeline_id)
+        partition_suffix = f"_tid{trigger_id}"
+        partition_stmt = f"FOR VALUES IN ({trigger_id})"
+        trigger_partition = pipeline_partition.create_partition(partition_suffix, partition_stmt=partition_stmt, subpartition_by="sample_key", subpartition_type="HASH")
+
+        # Create hash subpartitions
+
+        modulus = 16 # make configurable
+
+        for i in range(16):
+            partition_suffix = f"_part{i}"
+            partition_stmt = f"FOR VALUES WITH (modulus {modulus}, remainder {i})"
+            trigger_partition.create_partition(partition_suffix, partition_stmt=partition_stmt)
