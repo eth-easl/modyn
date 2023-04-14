c394220f132b9a389e7ac98a599cffd7ff532101
francescodeaglio <francesco.deaglio@gmail.com>
2023-04-14 16:31:24 +0200

    Introduction of remote gradnorm downsampling

--- modyn/trainer_server/internal/trainer/remote_downsamplers/__init__.py
+++ modyn/trainer_server/internal/trainer/remote_downsamplers/__init__.py
@@ -1,6 +1,8 @@
 import os
 
+from .remote_gradnorm_downsample import RemoteGradNormDownsampling  # noqa: F401
 from .remote_loss_downsample import RemoteLossDownsampling  # noqa: F401
+
 
 files = os.listdir(os.path.dirname(__file__))
 files.remove("__init__.py")
---
+++ modyn/trainer_server/internal/trainer/remote_downsamplers/remote_gradnorm_downsample.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+import torch
+from modyn.trainer_server.internal.trainer.remote_downsamplers.abstract_remote_downsample_strategy import (
+    AbstractRemoteDownsamplingStrategy,
+)
+
+
+class RemoteGradNormDownsampling(AbstractRemoteDownsamplingStrategy):
+    def __init__(self, params_from_selector: dict, per_sample_loss: Any, num_classes: int = -1) -> None:
+        super().__init__(params_from_selector)
+
+        self.per_sample_loss_fct = per_sample_loss
+        self.num_classes = num_classes
+
+    def get_probabilities(self, forward_output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        if isinstance(self.per_sample_loss_fct, torch.nn.CrossEntropyLoss) and self.num_classes > 0:
+            # no need to autograd if cross entropy loss is used since closed form solution exists
+            with torch.inference_mode():
+                probs = torch.nn.functional.softmax(forward_output, dim=1)
+                one_hot_targets = torch.nn.functional.one_hot(target, num_classes=self.num_classes)
+                scores = torch.norm(probs - one_hot_targets, dim=-1)
+        else:
+            sample_losses = self.per_sample_loss_fct(forward_output, target)
+            last_layer_gradients = torch.autograd.grad(sample_losses.sum(), forward_output, retain_graph=False)[0]
+            scores = torch.norm(last_layer_gradients, dim=-1)
+
+        return scores
