797adaf9908cbc48a3bf6cad7c9e2a25809366ae
Robin Oester <robin.oester@gmail.com>
2023-04-17 09:45:06 +0100

    Initial commit on the model storage component * added proto definitions * added basic setup similar to other components * added configuration options

---
+++ modyn/model_storage/model_storage.py
@@ -0,0 +1,26 @@
+import os
+import pathlib
+from typing import Optional, Tuple
+
+from jsonschema import ValidationError
+
+from modyn.utils import validate_yaml
+
+
+class ModelStorage:
+    def __init__(self, config: dict) -> None:
+        self.config = config
+
+        valid, errors = self._validate_config()
+        if not valid:
+            raise ValueError(f"Invalid configuration: {errors}")
+
+    def _validate_config(self) -> Tuple[bool, Optional[ValidationError]]:
+        schema_path = (
+                pathlib.Path(os.path.abspath(__file__)).parent.parent / "config" / "schema" / "modyn_config_schema.yaml"
+        )
+        return validate_yaml(self.config, schema_path)
+
+    def run(self) -> None:
+        print("Running Model Storage Component")
+        # run the model storage component
---
+++ modyn/model_storage/model_storage_entrypoint.py
@@ -0,0 +1,50 @@
+"""Entrypoint for the model storage service."""
+
+import argparse
+import logging
+import pathlib
+
+import yaml
+
+from modyn.model_storage.model_storage import ModelStorage
+
+logging.basicConfig(
+    level=logging.NOTSET,
+    format="[%(asctime)s]  [%(filename)15s:%(lineno)4d] %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d:%H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+
+def setup_argparser() -> argparse.ArgumentParser:
+    """Set up the argument parser.
+
+    Returns:
+        argparse.ArgumentParser: Argument parser
+    """
+    parser_ = argparse.ArgumentParser(description="Modyn Model Storage")
+    parser_.add_argument("config", type=pathlib.Path, action="store", help="Modyn infrastructure configuration file")
+
+    return parser_
+
+
+def main() -> None:
+    """Entrypoint for the model storage service."""
+    parser = setup_argparser()
+    args = parser.parse_args()
+
+    assert args.config.is_file(), f"File does not exist: {args.config}"
+
+    with open(args.config, "r", encoding="utf-8") as config_file:
+        modyn_config = yaml.safe_load(config_file)
+
+    logger.info("Initializing model storage.")
+    model_storage = ModelStorage(modyn_config)
+    logger.info("Starting model storage.")
+    model_storage.run()
+
+    logger.info("Model storage returned, exiting.")
+
+
+if __name__ == "__main__":
+    main()
