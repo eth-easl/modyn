13a1e8307c32d8833bbd09ebcdbfe985a4c4c4be
Maximilian Böther <maxi+git@boether.de>
2023-04-19 15:52:10 +0200

    ADD NULLABLE

--- modyn/storage/internal/database/models/sample.py
+++ modyn/storage/internal/database/models/sample.py
@@ -15,7 +15,7 @@ BIGINT = BigInteger().with_variant(sqlite.INTEGER(), "sqlite")
 
 class SampleMixin:
     sample_id = Column("sample_id", BIGINT, autoincrement=True, primary_key=True)
-    dataset_id = Column(Integer, ForeignKey("datasets.dataset_id"))
+    dataset_id = Column(Integer, ForeignKey("datasets.dataset_id"), nullable = False)
     file_id = Column(Integer, ForeignKey("files.file_id"), index=True)
     index = Column(BigInteger, nullable=False)
     label = Column(BigInteger, nullable=True)
