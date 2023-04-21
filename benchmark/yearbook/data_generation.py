7d3c33d5d00ee49f780a3d95ed0079ce80cda218
francescodeaglio <francesco.deaglio@gmail.com>
2023-04-21 16:12:31 +0200

    Refactor

--- benchmark/yearbook/data_generation.py
+++ benchmark/yearbook/data_generation.py
@@ -3,10 +3,25 @@ import pickle
 
 import numpy as np
 import torch
-from benchmark.download_utils import maybe_download
 from torch.utils.data import Dataset
+import pathlib
+import gdown
 
-
+def maybe_download(drive_id, destination_dir, destination_file_name):
+    """
+    Function to download data from Google Drive. Used for Wild-time based benchmarks.
+    This function is adapted from wild-time-data
+    """
+    destination_dir = pathlib.Path(destination_dir)
+    destination = destination_dir / destination_file_name
+    if destination.exists():
+        return
+    destination_dir.mkdir(parents=True, exist_ok=True)
+    gdown.download(
+        url=f"https://drive.google.com/u/0/uc?id={drive_id}&export=download&confirm=pbef",
+        output=str(destination),
+        quiet=False,
+    )
 class YearbookDownloader(Dataset):
     time_steps = [i for i in range(1930, 2014)]
     input_dim = (1, 32, 32)
