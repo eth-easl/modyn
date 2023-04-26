9e870b2afc7518a4978f650404426450bb25a6f7
Maximilian Böther <maxi+git@boether.de>
2023-04-26 20:38:09 +0200

    fix script

--- run_integrationtests.sh
+++ run_integrationtests.sh
@@ -1,4 +1,6 @@
 docker compose down
 docker build -t modynbase -f docker/Base/Dockerfile .
 docker compose up --build tests --abort-on-container-exit --exit-code-from tests
+exitcode=$?
 docker compose down
+exit $exitcode
