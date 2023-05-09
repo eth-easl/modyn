6fc869eaab8ad29da47f747b7924aed5f3c1c005
Robin Oester <32065379+robin-oester@users.noreply.github.com>
2023-05-09 11:35:39 +0200

    Add Model Storage (#232)
    Implementing model storage component based on #167. Currently, trained
    models are stored on the supervisor which are downloaded from the
    trainer server. We want to have a separate component storing the models.
    For the supervisor it should then be possible to continue training from
    a stored model or to evaluate it.
    
    ---------
    
    Co-authored-by: Maximilian Böther <2116466+MaxiBoether@users.noreply.github.com>

---
+++ modyn/common/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module contains classes shared among several components.
+"""
+# TODO(#243): extend package with additional classes and functions
+
+import os
+
+files = os.listdir(os.path.dirname(__file__))
+files.remove("__init__.py")
+__all__ = [f[:-3] for f in files if f.endswith(".py")]
