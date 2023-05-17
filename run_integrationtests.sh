c5e013ed5bcf993beb967e7b8a45cfabda946b52
Maximilian Böther <maxi+git@boether.de>
2023-05-17 09:44:07 +0200

    start working on multi stage build for apex

--- run_integrationtests.sh
+++ run_integrationtests.sh
@@ -8,6 +8,7 @@ if [[ ! -z "$CI" ]]; then
     cp default_postgresql.conf storage_postgresql.conf
 fi
 
+docker build -t modyndependencies -f docker/Dependencies/Dockerfile .
 docker build -t modynbase -f docker/Base/Dockerfile .
 docker compose up --build tests --abort-on-container-exit --exit-code-from tests
 exitcode=$?
