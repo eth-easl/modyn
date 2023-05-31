5c127646010ab2a6cdb651f904e60d4c1a8975d6
Viktor Gsteiger <vgsteiger@student.ethz.ch>
2023-05-31 11:23:37 +0200

    Fix unittests?

--- modyn/storage/__init__.py
+++ modyn/storage/__init__.py
@@ -5,8 +5,6 @@ The storage module contains all classes and functions related to the storage and
 
 import os
 
-from .storage import Storage  # noqa: F401
-
 files = os.listdir(os.path.dirname(__file__))
 files.remove("__init__.py")
 __all__ = [f[:-3] for f in files if f.endswith(".py")]
