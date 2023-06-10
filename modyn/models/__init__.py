03e42801359295600df84b9aa6331d514e07edc0
francescodeaglio <francesco.deaglio@gmail.com>
2023-06-10 13:25:43 +0200

    Undo wrong commit

--- modyn/models/__init__.py
+++ modyn/models/__init__.py
@@ -4,9 +4,7 @@
 import os
 
 from .dlrm.dlrm import DLRM  # noqa: F401
-from .fmownet.fmownet import FmowNet  # noqa: F401
 from .resnet18.resnet18 import ResNet18  # noqa: F401
-from .yearbooknet.yearbooknet import YearbookNet  # noqa: F401
 
 files = os.listdir(os.path.dirname(__file__))
 files.remove("__init__.py")
