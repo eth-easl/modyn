b5c1555f7442bc1b0dffdf470e22a3bb73ee4522
francescodeaglio <francesco.deaglio@gmail.com>
2023-06-22 12:22:47 +0200

    Basic documentation

--- modyn/storage/internal/file_wrapper/csv_file_wrapper.py
+++ modyn/storage/internal/file_wrapper/csv_file_wrapper.py
@@ -24,6 +24,7 @@ class CsvFileWrapper(AbstractFileWrapper):
             )
         self.label_index = file_wrapper_config["label_index"]
 
+        # the first line might contain the header, which is useless and must not be returned.
         if "ignore_first_line" in file_wrapper_config:
             self.ignore_first_line = file_wrapper_config["ignore_first_line"]
         else:
@@ -34,7 +35,9 @@ class CsvFileWrapper(AbstractFileWrapper):
         else:
             self.encoding = "utf-8"
 
+        # check that the file is actually a CSV
         self._validate_file_extension()
+
         self._validate_file_content()
 
     def _validate_file_extension(self) -> None:
@@ -47,6 +50,14 @@ class CsvFileWrapper(AbstractFileWrapper):
             raise ValueError("File has wrong file extension.")
 
     def _validate_file_content(self):
+        """
+        Performs the following checks:
+        - specified label column is castable to integer
+        - each row has the label_index_column
+        - each row has the same width
+
+        Raises a ValueError if a condition is not met
+        """
 
         reader = self._get_csv_reader()
 
@@ -58,13 +69,14 @@ class CsvFileWrapper(AbstractFileWrapper):
             if self.label_index is not None:
                 if not 0 <= self.label_index < len(row):
                     raise ValueError("Label index outside row boundary")
-                if not row[self.label_index].isnumeric():
+                if not row[self.label_index].isnumeric(): #returns true iff all the characters are numbers
                     raise ValueError("The label must be an integer")
 
         if not len(set(number_of_columns)) == 1:
             raise ValueError("Some rows have different width. "
                              f"This is the number of columns row by row {number_of_columns}")
     def get_sample(self, index: int) -> bytes:
+
         samples = self._filter_rows_samples([index])
 
         if len(samples) != 1:
@@ -116,6 +128,11 @@ class CsvFileWrapper(AbstractFileWrapper):
         return sum(1 for _ in reader)
 
     def _get_csv_reader(self) -> Iterator:
+        """
+        Receives the bytes from the file_system_wrapper and creates a csv.reader out of it.
+        Returns:
+            csv.reader
+        """
         data_file = self.filesystem_wrapper.get(self.file_path)
 
         # Convert bytes content to a string
@@ -133,6 +150,15 @@ class CsvFileWrapper(AbstractFileWrapper):
         return reader
 
     def _filter_rows_samples(self, indices: list[int]) -> list[bytes]:
+        """
+        Filters the selected rows and removes the label column
+        Args:
+            indices: list of rows that must be kept
+
+        Returns:
+            list of byte-encoded rows
+
+        """
         reader = self._get_csv_reader()
 
         # Iterate over the rows and keep the selected ones
@@ -141,11 +167,21 @@ class CsvFileWrapper(AbstractFileWrapper):
             if i in indices:
                 # Remove the label, convert the row to bytes and append to the list
                 row_without_label = [col for j, col in enumerate(row) if j != self.label_index]
+                # the row is transformed in a similar csv using the same separator and then transformed to bytes
                 filtered_rows.append(bytes(self.separator.join(row_without_label), self.encoding))
 
         return filtered_rows
 
     def _filter_rows_labels(self, indices: list[int]) -> list[int]:
+        """
+        Filters the selected rows and extracts the label column
+        Args:
+            indices: list of rows that must be kept
+
+        Returns:
+            list of labels
+
+        """
         reader = self._get_csv_reader()
 
         # Iterate over the rows and keep the selected ones
