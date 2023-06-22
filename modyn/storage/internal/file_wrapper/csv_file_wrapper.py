c71c8a87677a80431687a92fe7323156727046eb
francescodeaglio <francesco.deaglio@gmail.com>
2023-06-22 10:42:03 +0200

    CSV file wrapper introduction

---
+++ modyn/storage/internal/file_wrapper/csv_file_wrapper.py
@@ -0,0 +1,127 @@
+import csv
+from typing import Iterator, Optional
+
+from modyn.storage.internal.file_wrapper.abstract_file_wrapper import AbstractFileWrapper
+from modyn.storage.internal.file_wrapper.file_wrapper_type import FileWrapperType
+from modyn.storage.internal.filesystem_wrapper.abstract_filesystem_wrapper import AbstractFileSystemWrapper
+
+
+class CsvFileWrapper(AbstractFileWrapper):
+    def __init__(self, file_path: str, file_wrapper_config: dict, filesystem_wrapper: AbstractFileSystemWrapper):
+        super().__init__(file_path, file_wrapper_config, filesystem_wrapper)
+
+        self.file_wrapper_type = FileWrapperType.CsvFileWrapper
+
+        if "separator" in file_wrapper_config:
+            self.separator = file_wrapper_config["separator"]
+        else:
+            self.separator = ";"
+
+        if "label_column" not in file_wrapper_config:
+            raise ValueError(
+                "Please specify the index of the column that contains the label. "
+                "Use None if no column contains the label"
+            )
+        self.label_index = file_wrapper_config["label_column"]
+
+        if "ignore_header" in file_wrapper_config:
+            self.ignore_header = file_wrapper_config["ignore_header"]
+        else:
+            self.ignore_header = False
+
+        if "encoding" in file_wrapper_config:
+            self.encoding = file_wrapper_config["encoding"]
+        else:
+            self.encoding = "utf-8"
+
+        self._validate_file_extension()
+
+    def _validate_file_extension(self) -> None:
+        """Validates the file extension as csv
+
+        Raises:
+            ValueError: File has wrong file extension
+        """
+        if not self.file_path.endswith(".csv"):
+            raise ValueError("File has wrong file extension.")
+
+    def get_sample(self, index: int) -> bytes:
+        samples = self._filter_rows_samples([index])
+        assert len(samples) == 1
+
+        return samples[0]
+
+    def get_samples(self, start: int, end: int) -> list[bytes]:
+        indices = list(range(start, end))
+        return self.get_samples_from_indices(indices)
+
+    def get_samples_from_indices(self, indices: list) -> list[bytes]:
+        samples = self._filter_rows_samples(indices)
+        assert len(samples) == len(indices)
+
+        return samples
+
+    def get_label(self, index: int) -> Optional[int]:
+        if self.label_index is None:
+            return None
+
+        labels = self._filter_rows_labels([index])
+        assert len(labels) == 0
+        return labels[0]
+
+    def get_all_labels(self) -> list[Optional[int]]:
+        reader = self._get_csv_reader()
+
+        labels = []
+        for row in reader:
+            only_label = row[self.label_index]
+            labels.append(only_label)
+
+        return labels
+
+    def get_number_of_samples(self) -> int:
+        reader = self._get_csv_reader()
+
+        return sum(1 for _ in reader)
+
+    def _get_csv_reader(self) -> Iterator:
+        data_file = self.filesystem_wrapper.get(self.file_path)
+
+        # Convert bytes content to a string
+        data_file_str = data_file.decode(self.encoding)
+
+        lines = data_file_str.split("\n")
+
+        # Create a CSV reader
+        reader = csv.reader(lines, delimiter=self.separator)
+
+        # skip the header if required
+        if self.ignore_header:
+            next(reader)
+
+        return reader
+
+    def _filter_rows_samples(self, indices: list[int]) -> list[bytes]:
+        reader = self._get_csv_reader()
+
+        # Iterate over the rows and keep the selected ones
+        filtered_rows = []
+        for i, row in enumerate(reader):
+            if i in indices:
+                # Remove the label, convert the row to bytes and append to the list
+                row_without_label = [col for j, col in enumerate(row) if j != self.label_index]
+                filtered_rows.append(bytes(self.separator.join(row_without_label), self.encoding))
+
+        return filtered_rows
+
+    def _filter_rows_labels(self, indices: list[int]) -> list[int]:
+        reader = self._get_csv_reader()
+
+        # Iterate over the rows and keep the selected ones
+        filtered_rows = []
+        for i, row in enumerate(reader):
+            if i in indices:
+                only_label = row[self.label_index]
+                filtered_rows.append(only_label)
+
+        return filtered_rows
