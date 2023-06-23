78af088ee65bacd1ba4cb3798821a10a5065daf2
francescodeaglio <francesco.deaglio@gmail.com>
2023-06-23 15:55:42 +0200

    Comment on index_sampleid_map

--- modyn/trainer_server/internal/trainer/remote_downsamplers/abstract_remote_downsample_strategy.py
+++ modyn/trainer_server/internal/trainer/remote_downsamplers/abstract_remote_downsample_strategy.py
@@ -35,6 +35,14 @@ class AbstractRemoteDownsamplingStrategy(ABC):
         self._sampling_concluded = False
 
         self.replacement = params_from_selector.get("replacement", True)
+
+        # The next variable is used to keep a mapping index <-> sample_id
+        # This is needed since the data selection policy works on indexes (the policy does not care what the sample_id
+        # is, it simply stores its score in a vector/matrix) but for retrieving again the data we need somehow to
+        # remember the sample_id. So, index_sampleid_map might cantain something like [124, 156, 562, 18] and the
+        # per-sample score (whatever it is, Gradnom/loss/CRAIG..) be [1.23, 0.31, 14.3, 0.09]. So, for example, the
+        # policy selects the two points with highest score ([0, 2]) and we need to know that 0 is sample 124 and 2 is
+        # sample 562.
         self.index_sampleid_map: list[int] = []
 
     def get_downsampling_ratio(self) -> int:
