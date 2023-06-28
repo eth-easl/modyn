15f1ae3e6226fe033aa49a54b033b0378445a465
francescodeaglio <francesco.deaglio@gmail.com>
2023-06-28 09:50:47 +0200

    Resnet torchvision simplified

--- modyn/models/resnet18/resnet18.py
+++ modyn/models/resnet18/resnet18.py
@@ -1,11 +1,11 @@
 from typing import Any
 
 # torchvision resnet updated to add the embedding recorder.
-from modyn.models.resnet18.resnet_torchvision import resnet18
+from modyn.models.resnet18.resnet_torchvision import ResNetModyn
 
 
 class ResNet18:
     # pylint: disable-next=unused-argument
     def __init__(self, model_configuration: dict[str, Any], device: str, amp: bool) -> None:
-        self.model = resnet18(**model_configuration)
+        self.model = ResNetModyn(model_configuration)
         self.model.to(device)
