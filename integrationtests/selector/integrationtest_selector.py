8967e10cfba3d1fca525ced03e5167d31fafb94e
francescodeaglio <francesco.deaglio@gmail.com>
2023-07-04 11:20:07 +0200

    Integration tests

--- integrationtests/selector/integrationtest_selector.py
+++ integrationtests/selector/integrationtest_selector.py
@@ -28,6 +28,261 @@ def connect_to_selector_servicer() -> grpc.Channel:
     return selector_channel
 
 
+def test_label_balanced_presampling_huge() -> None:
+    selector_channel = connect_to_selector_servicer()
+    selector = SelectorStub(selector_channel)
+
+    strategy_config = {
+        "name": "CoresetStrategy",
+        "maximum_keys_in_memory": 250,
+        "config": {
+            "limit": -1,
+            "reset_after_trigger": True,
+            "presampling_config": {"strategy": "LabelBalancedPresamplingStrategy", "ratio": 50},
+        },
+    }
+
+    pipeline_id = selector.register_pipeline(
+        RegisterPipelineRequest(num_workers=2, selection_strategy=JsonString(value=json.dumps(strategy_config)))
+    ).pipeline_id
+
+    trigger_id = selector.inform_data_and_trigger(
+        DataInformRequest(
+            pipeline_id=pipeline_id,
+            keys=list(range(10, 5010)),
+            timestamps=list(range(100000, 105000)),
+            labels=[0, 1] * 2500,
+        )
+    ).trigger_id
+
+    number_of_partitions = selector.get_number_of_partitions(
+        GetNumberOfPartitionsRequest(pipeline_id=pipeline_id, trigger_id=trigger_id)
+    ).num_partitions
+
+    # 5000 samples (reset after trigger) , 2500 sampled, 10 partitions
+    assert number_of_partitions == 10, f"Invalid number of partitions: {number_of_partitions}"
+    total_samples = []
+
+    for partition in range(number_of_partitions):
+        worker1_responses: list[SamplesResponse] = list(
+            selector.get_sample_keys_and_weights(
+                GetSamplesRequest(pipeline_id=pipeline_id, trigger_id=trigger_id, worker_id=0, partition_id=partition)
+            )
+        )
+
+        worker2_responses: list[SamplesResponse] = list(
+            selector.get_sample_keys_and_weights(
+                GetSamplesRequest(pipeline_id=pipeline_id, trigger_id=trigger_id, worker_id=1, partition_id=partition)
+            )
+        )
+
+        worker1_response = worker1_responses[0]
+        worker2_response = worker2_responses[0]
+
+        worker_1_samples = list(worker1_response.training_samples_subset)
+        worker_2_samples = list(worker2_response.training_samples_subset)
+
+        assert len(worker_1_samples) == 125, f"Received {len(worker_1_samples)} samples instead of 125."
+        assert len(worker_2_samples) == 125, f"Received {len(worker_2_samples)} samples instead of 125."
+
+        worker_1_weights = list(worker1_response.training_samples_weights)
+        worker_2_weights = list(worker2_response.training_samples_weights)
+        assert len(worker_1_samples) == len(worker_1_weights)
+        assert len(worker_2_samples) == len(worker_2_weights)
+
+        total_samples.extend(worker_1_samples + worker_2_samples)
+
+    assert len(total_samples) == len(set(total_samples)), "Received duplicated samples"
+    assert set(total_samples) <= set(
+        range(10000)
+    ), f"Got samples with out of range keys: {set(total_samples) - set(range(10000))}"
+    assert len(total_samples) == 2500, f"expected 2000 samples, got {len(total_samples)}"
+
+    # count of samples with label 1
+    assert sum(1 if el % 2 == 0 else 0 for el in total_samples) == 1250
+    # count of samples with label 0
+    assert sum(1 if el % 2 == 1 else 0 for el in total_samples) == 1250
+
+
+def test_label_balanced_force_same_size():
+    selector_channel = connect_to_selector_servicer()
+    selector = SelectorStub(selector_channel)
+
+    strategy_config = {
+        "name": "CoresetStrategy",
+        "maximum_keys_in_memory": 100,
+        "config": {
+            "limit": -1,
+            "reset_after_trigger": False,
+            "presampling_config": {
+                "strategy": "LabelBalancedPresamplingStrategy",
+                "ratio": 90,
+                "force_label_balancing": True,
+            },
+        },
+    }
+
+    pipeline_id = selector.register_pipeline(
+        RegisterPipelineRequest(num_workers=2, selection_strategy=JsonString(value=json.dumps(strategy_config)))
+    ).pipeline_id
+
+    # now we just have 2 classes with 4 samples each
+    selector.inform_data(
+        DataInformRequest(
+            pipeline_id=pipeline_id,
+            keys=[0, 1, 2],
+            timestamps=[1, 2, 3],
+            labels=[1, 0, 1],
+        )
+    )
+
+    trigger_id = selector.inform_data_and_trigger(
+        DataInformRequest(
+            pipeline_id=pipeline_id,
+            keys=[3, 4, 5, 6, 7],
+            timestamps=[4, 5, 6, 7, 8],
+            labels=[0, 1, 0, 1, 0],
+        )
+    ).trigger_id
+
+    number_of_partitions = selector.get_number_of_partitions(
+        GetNumberOfPartitionsRequest(pipeline_id=pipeline_id, trigger_id=trigger_id)
+    ).num_partitions
+
+    # We have 8 samples, 4 for each class. 90% presampling means 6 samples per class hence 1 partition
+    assert number_of_partitions == 1, f"Invalid number of partitions: {number_of_partitions}"
+
+    total_samples = _get_samples(pipeline_id, selector, trigger_id, 0, [3, 3])
+
+    assert len(total_samples) == 6
+    # 2 samples with label 0
+    assert len(set(total_samples).intersection({1, 3, 5, 7})) == 3
+    # 2 samples with label 1
+    assert len(set(total_samples).intersection({0, 2, 4, 6})) == 3
+
+    # now let's add a third, smaller class with just two samples
+    trigger_id = selector.inform_data_and_trigger(
+        DataInformRequest(
+            pipeline_id=pipeline_id,
+            keys=[100, 101],
+            timestamps=[9, 10],
+            labels=[2, 2],
+        )
+    ).trigger_id
+
+    number_of_partitions = selector.get_number_of_partitions(
+        GetNumberOfPartitionsRequest(pipeline_id=pipeline_id, trigger_id=trigger_id)
+    ).num_partitions
+
+    # We have 10 samples, [4,4,2] for each class. Since we want strong balance, each class should have 2 samples
+    assert number_of_partitions == 1, f"Invalid number of partitions: {number_of_partitions}"
+
+    total_samples = _get_samples(pipeline_id, selector, trigger_id, 0, [3, 3])
+
+    assert len(total_samples) == 6
+    # 2 samples with label 0
+    assert len(set(total_samples).intersection({1, 3, 5, 7})) == 2
+    # 2 samples with label 1
+    assert len(set(total_samples).intersection({0, 2, 4, 6})) == 2
+    # 2 samples with label 2
+    assert len(set(total_samples).intersection({100, 101})) == 2
+
+
+def test_label_balanced_force_all_samples():
+    selector_channel = connect_to_selector_servicer()
+    selector = SelectorStub(selector_channel)
+
+    strategy_config = {
+        "name": "CoresetStrategy",
+        "maximum_keys_in_memory": 100,
+        "config": {
+            "limit": -1,
+            "reset_after_trigger": False,
+            "presampling_config": {
+                "strategy": "LabelBalancedPresamplingStrategy",
+                "ratio": 90,
+                "force_required_target_size": True,
+            },
+        },
+    }
+
+    pipeline_id = selector.register_pipeline(
+        RegisterPipelineRequest(num_workers=2, selection_strategy=JsonString(value=json.dumps(strategy_config)))
+    ).pipeline_id
+
+    # same classes as before
+    selector.inform_data(
+        DataInformRequest(
+            pipeline_id=pipeline_id,
+            keys=[0, 1, 2],
+            timestamps=[1, 2, 3],
+            labels=[1, 0, 1],
+        )
+    )
+
+    selector.inform_data(
+        DataInformRequest(
+            pipeline_id=pipeline_id,
+            keys=[3, 4, 5, 6, 7],
+            timestamps=[4, 5, 6, 7, 8],
+            labels=[0, 1, 0, 1, 0],
+        )
+    )
+
+    trigger_id = selector.inform_data_and_trigger(
+        DataInformRequest(
+            pipeline_id=pipeline_id,
+            keys=[100, 101],
+            timestamps=[9, 10],
+            labels=[2, 2],
+        )
+    ).trigger_id
+
+    number_of_partitions = selector.get_number_of_partitions(
+        GetNumberOfPartitionsRequest(pipeline_id=pipeline_id, trigger_id=trigger_id)
+    ).num_partitions
+
+    # We have 10 samples, [4,4,2] for each class. Target is 9 (90%)
+    # We want to get exactly 9 samples
+    assert number_of_partitions == 1, f"Invalid number of partitions: {number_of_partitions}"
+
+    total_samples = _get_samples(pipeline_id, selector, trigger_id, 0, [5, 4])
+
+    assert len(total_samples) == 9
+
+    # 3 or 4 samples of class 0 (there is an extra sample to achieve exactly 9)
+    assert 3 <= len(set(total_samples).intersection({1, 3, 5, 7})) <= 4
+    # 3 or 4 samples of class 1 (there is an extra sample to achieve exactly 9)
+    assert 3 <= len(set(total_samples).intersection({0, 2, 4, 6})) <= 4
+    # 1 or 2 samples with label 2 (there is an extra sample to achieve exactly 9)
+    assert 1 <= len(set(total_samples).intersection({100, 101})) <= 2
+
+
+def _get_samples(pipeline_id, selector, trigger_id, partition_id, expected_len):
+    worker1_responses: list[SamplesResponse] = list(
+        selector.get_sample_keys_and_weights(
+            GetSamplesRequest(pipeline_id=pipeline_id, trigger_id=trigger_id, worker_id=0, partition_id=partition_id)
+        )
+    )
+    worker2_responses: list[SamplesResponse] = list(
+        selector.get_sample_keys_and_weights(
+            GetSamplesRequest(pipeline_id=pipeline_id, trigger_id=trigger_id, worker_id=1, partition_id=partition_id)
+        )
+    )
+    worker1_response = worker1_responses[0]
+    worker2_response = worker2_responses[0]
+    worker_1_samples = list(worker1_response.training_samples_subset)
+    worker_2_samples = list(worker2_response.training_samples_subset)
+    assert (
+        len(worker_1_samples) == expected_len[0]
+    ), f"Received {len(worker_1_samples)} samples instead of {expected_len[0]}."
+    assert (
+        len(worker_2_samples) == expected_len[1]
+    ), f"Received {len(worker_1_samples)} samples instead of {expected_len[1]}."
+    total_samples = worker_1_samples + worker_2_samples
+    return total_samples
+
+
 def test_newdata() -> None:
     selector_channel = connect_to_selector_servicer()
     selector = SelectorStub(selector_channel)
@@ -629,6 +884,9 @@ def test_many_samples_unevenly_distributed():
 
 if __name__ == "__main__":
     test_newdata()
+    test_label_balanced_presampling_huge()
+    test_label_balanced_force_same_size()
+    test_label_balanced_force_all_samples()
     test_empty_triggers()
     test_many_samples_evenly_distributed()
     test_many_samples_unevenly_distributed()
