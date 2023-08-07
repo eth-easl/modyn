cbae4fd12832a1be2fbb22100faa06096fc72e0f
francescodeaglio <francesco.deaglio@gmail.com>
2023-08-07 11:32:04 +0200

    Docstrings

--- modyn/models/coreset_methods_support.py
+++ modyn/models/coreset_methods_support.py
@@ -29,6 +29,21 @@ class EmbeddingRecorder(nn.Module):
 
 
 class CoresetMethodsSupport(nn.Module, ABC):
+    """
+    This class is used to support some Coreset Methods.
+    Embeddings, here defined as the activation before the last layer, are often used to estimate the importance of
+    a point. To implement this class correctly, it is necessary to
+        - implement the get_last_layer method
+        - modify the forward pass so that the last layer embedding is recorded. For example, in a simple network like
+            x = self.fc1(input)
+            x = self.fc2(x)
+            output = self.fc3(x)
+        it must be modified as follows
+            x = self.fc1(input)
+            x = self.fc2(x)
+            x = self.embedding_recorder(x)
+            output = self.fc3(x)
+    """
     def __init__(self, record_embedding: bool = False) -> None:
         super().__init__()
         self.embedding_recorder = EmbeddingRecorder(record_embedding)
@@ -40,4 +55,8 @@ class CoresetMethodsSupport(nn.Module, ABC):
 
     @abstractmethod
     def get_last_layer(self) -> nn.Module:
+        """
+        Returns the last layer. Used for example to obtain the pre-layer and post-layer dimensions of tensors
+
+        """
         raise NotImplementedError()
