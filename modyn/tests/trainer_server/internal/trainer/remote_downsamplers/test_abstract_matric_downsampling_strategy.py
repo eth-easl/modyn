233c77f73d157a5582399abdd91e572bce14ef4c
francescodeaglio <francesco.deaglio@gmail.com>
2023-08-08 17:55:11 +0200

    Add Matrix based downsampling strategy

---
+++ modyn/tests/trainer_server/internal/trainer/remote_downsamplers/test_abstract_matric_downsampling_strategy.py
@@ -0,0 +1,79 @@
+# pylint: disable=abstract-class-instantiated
+from unittest.mock import patch
+
+import numpy as np
+import torch
+from modyn.trainer_server.internal.trainer.remote_downsamplers.abstract_matrix_downsampling_strategy import (
+    AbstractMatrixDownsamplingStrategy,
+    MatrixContent,
+)
+
+
+def get_sampler_config():
+    downsampling_ratio = 50
+    per_sample_loss_fct = torch.nn.CrossEntropyLoss(reduction="none")
+
+    params_from_selector = {"downsampling_ratio": downsampling_ratio, "sample_then_batch": False, "args": {}}
+    return 0, 0, 0, params_from_selector, per_sample_loss_fct
+
+
+@patch.multiple(AbstractMatrixDownsamplingStrategy, __abstractmethods__=set())
+def test_init():
+    amds = AbstractMatrixDownsamplingStrategy(*get_sampler_config())
+
+    assert amds.requires_coreset_methods_support
+    assert not amds.matrix_elements
+    assert amds.matrix_content is None
+
+
+@patch.multiple(AbstractMatrixDownsamplingStrategy, __abstractmethods__=set())
+def test_collect_embeddings():
+    amds = AbstractMatrixDownsamplingStrategy(*get_sampler_config())
+
+    amds.matrix_content = MatrixContent.EMBEDDINGS
+
+    assert amds.requires_coreset_methods_support
+    assert not amds.matrix_elements  # thank you pylint! amds.matrix_elements == []
+
+    first_embedding = torch.randn((4, 5))
+    second_embedding = torch.randn((3, 5))
+    amds.inform_samples([1, 2, 3, 4], None, None, first_embedding)
+    amds.inform_samples([21, 31, 41], None, None, second_embedding)
+
+    assert np.concatenate(amds.matrix_elements).shape == (7, 5)
+    assert amds.matrix_elements == [first_embedding, second_embedding]
+    assert amds.index_sampleid_map == [1, 2, 3, 4, 21, 31, 41]
+
+    third_embedding = torch.randn((23, 5))
+    amds.inform_samples(list(range(1000, 1023)), None, None, third_embedding)
+
+    assert np.concatenate(amds.matrix_elements).shape == (30, 5)
+    assert amds.matrix_elements == [first_embedding, second_embedding, third_embedding]
+    assert amds.index_sampleid_map == [1, 2, 3, 4, 21, 31, 41] + list(range(1000, 1023))
+
+
+@patch.multiple(AbstractMatrixDownsamplingStrategy, __abstractmethods__=set())
+def test_collect_gradients():
+    amds = AbstractMatrixDownsamplingStrategy(*get_sampler_config())
+    amds.matrix_content = MatrixContent.GRADIENTS
+
+    first_output = torch.randn((4, 2))
+    first_output.requires_grad = True
+    first_target = torch.tensor([1, 1, 1, 0])
+    first_embedding = torch.randn((4, 5))
+    amds.inform_samples([1, 2, 3, 4], first_output, first_target, first_embedding)
+
+    second_output = torch.randn((3, 2))
+    second_output.requires_grad = True
+    second_target = torch.tensor([0, 1, 0])
+    second_embedding = torch.randn((3, 5))
+    amds.inform_samples([21, 31, 41], second_output, second_target, second_embedding)
+
+    assert len(amds.matrix_elements) == 2
+
+    # expected shape = (a,b)
+    # a = 7 (4 samples in the first batch and 3 samples in the second batch)
+    # b = 5 * 2 + 2 where 5 is the input dimension of the last layer and 2 is the output one
+    assert np.concatenate(amds.matrix_elements).shape == (7, 12)
+
+    assert amds.index_sampleid_map == [1, 2, 3, 4, 21, 31, 41]
---
+++ modyn/trainer_server/internal/trainer/remote_downsamplers/abstract_matrix_downsampling_strategy.py
@@ -0,0 +1,76 @@
+from abc import abstractmethod
+from enum import Enum
+from typing import Any, Optional
+
+import numpy as np
+import torch
+from modyn.trainer_server.internal.trainer.remote_downsamplers.abstract_remote_downsampling_strategy import (
+    AbstractRemoteDownsamplingStrategy,
+)
+
+MatrixContent = Enum("MatrixContent", ["EMBEDDINGS", "GRADIENTS"])
+
+
+class AbstractMatrixDownsamplingStrategy(AbstractRemoteDownsamplingStrategy):
+    def __init__(
+        self, pipeline_id: int, trigger_id: int, batch_size: int, params_from_selector: dict, per_sample_loss: Any
+    ):
+        super().__init__(pipeline_id, trigger_id, batch_size, params_from_selector)
+
+        self.criterion = per_sample_loss
+
+        # This class uses the embedding recorder
+        self.requires_coreset_methods_support = True
+        self.matrix_elements: list[torch.Tensor] = []
+
+        self.matrix_content: Optional[MatrixContent] = None
+
+    def inform_samples(
+        self,
+        sample_ids: list[int],
+        forward_output: torch.Tensor,
+        target: torch.Tensor,
+        embedding: Optional[torch.Tensor] = None,
+    ) -> None:
+        assert embedding is not None
+        assert self.matrix_content is not None
+
+        if self.matrix_content == MatrixContent.GRADIENTS:
+            to_be_added = self._compute_gradients(forward_output, target, embedding)
+        elif self.matrix_content == MatrixContent.EMBEDDINGS:
+            to_be_added = embedding
+        else:
+            raise AssertionError("The required content does not exits.")
+
+        self.matrix_elements.append(to_be_added)
+        # keep the mapping index<->sample_id
+        self.index_sampleid_map += sample_ids
+
+    def _compute_gradients(
+        self, forward_output: torch.Tensor, target: torch.Tensor, embedding: torch.Tensor
+    ) -> torch.Tensor:
+        loss = self.criterion(forward_output, target).mean()
+        embedding_dim = embedding.shape[1]
+        num_classes = forward_output.shape[1]
+        batch_num = target.shape[0]
+        # compute the gradient for each element provided
+        with torch.no_grad():
+            bias_parameters_grads = torch.autograd.grad(loss, forward_output)[0]
+
+            weight_parameters_grads = embedding.view(batch_num, 1, embedding_dim).repeat(
+                1, num_classes, 1
+            ) * bias_parameters_grads.view(batch_num, num_classes, 1).repeat(1, 1, embedding_dim)
+            print(weight_parameters_grads.shape)
+            gradients = torch.cat([bias_parameters_grads, weight_parameters_grads.flatten(1)], dim=1).cpu().numpy()
+        return gradients
+
+    def select_points(self) -> tuple[list[int], torch.Tensor]:
+        number_of_samples = len(self.matrix_elements)
+        target_size = int(self.downsampling_ratio * number_of_samples / 100)
+
+        matrix = np.concatenate(self.matrix_elements)
+        return self._select_points_from_matrix(matrix, target_size)
+
+    @abstractmethod
+    def _select_points_from_matrix(self, matrix: np.ndarray, target_size: int) -> tuple[list[int], torch.Tensor]:
+        raise NotImplementedError()
