0814863ef081e98461396e13e486ab0109f372ed
Maximilian Böther <maxi+git@boether.de>
2023-09-25 11:30:31 +0200

    add some experiment files

---
+++ run_prefetch_exp.sh
@@ -0,0 +1,8 @@
+modyn-supervisor --start-replay-at 0 --maximum-triggers 0 prefetch0.yml modyn/config/examples/modyn_config.yml /modyn_host/eval/prefetch0
+modyn-supervisor --start-replay-at 0 --maximum-triggers 0 prefetch1.yml modyn/config/examples/modyn_config.yml /modyn_host/eval/prefetch1
+modyn-supervisor --start-replay-at 0 --maximum-triggers 0 prefetch4.yml modyn/config/examples/modyn_config.yml /modyn_host/eval/prefetch4
+modyn-supervisor --start-replay-at 0 --maximum-triggers 0 prefetch8.yml modyn/config/examples/modyn_config.yml /modyn_host/eval/prefetch8
+modyn-supervisor --start-replay-at 0 --maximum-triggers 0 prefetch8_4workers.yml modyn/config/examples/modyn_config.yml /modyn_host/eval/prefetch8_4workers
+
+
+
