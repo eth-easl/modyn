93f5ecd5550b748b0ec472a1fb35d17ea2325b29
Maximilian Böther <maxi+git@boether.de>
2023-10-04 17:43:18 +0200

    small fix

--- experiments/criteo_online_dataset/run_prefetch_exp.sh
+++ experiments/criteo_online_dataset/run_prefetch_exp.sh
@@ -4,7 +4,7 @@ BASEDIR="/modyn_host/eval/criteo_dataset_$(date +%s)"
 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-MODYN_CONFIG_PATH="$SCRIPT_DIR/../../modyn/config/examples/modyn_config.yml"
+MODYN_CONFIG_PATH="$SCRIPT_DIR/../../modyn/config/examples/modyn_config.yaml"
 
 for filename in $SCRIPT_DIR/pipelines/*.yml; do
     BASE=$(basename "$filename" | cut -d. -f1)
