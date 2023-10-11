087bb1caf2075f89e96e79262a2182888cbdfa12
Robin Oester <robin.oester@gmail.com>
2023-10-11 09:18:50 +0200

    Increase timeout threshold of availability test * from 60 to 90 seconds

--- integrationtests/test_docker_compose.py
+++ integrationtests/test_docker_compose.py
@@ -7,7 +7,7 @@ from integrationtests.utils import get_modyn_config
 from modyn.storage.internal.grpc.generated.storage_pb2_grpc import StorageStub  # noqa: F401
 from modyn.utils import grpc_connection_established
 
-TIMEOUT = 60  # seconds
+TIMEOUT = 90  # seconds
 
 
 def terminate_on_timeout(start_time: int) -> None:
