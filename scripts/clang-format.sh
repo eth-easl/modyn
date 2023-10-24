008c2fb50ac553907cfc417ebbc62c018659555f
Maximilian Böther <maxi+git@boether.de>
2023-10-24 17:09:20 +0200

    fix all the names

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 function run_format() {
     local subdir=$1
-    find "${DIR}"/"${subdir}" \( -iname '*.hpp' -o -iname '*.cpp' \) -print | awk '!/build/' | xargs clang-format -i
+    find "${PARENT_DIR}"/"${subdir}" \( -iname '*.hpp' -o -iname '*.cpp' \) -print | awk '!/build/' | xargs clang-format -i
 }
 
 echo "Running on storage directory"
