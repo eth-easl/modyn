8e82373044f35c6ed05af4ee2253bf4c992cd39a
vgsteiger <v.gsteiger@gmail.com>
2023-10-25 19:15:02 +0200

    Fix comma

--- modyn/storage/src/internal/database/sql/PostgreSQLFile.sql
+++ modyn/storage/src/internal/database/sql/PostgreSQLFile.sql
@@ -3,7 +3,7 @@ R"(CREATE TABLE IF NOT EXISTS files (
     dataset_id INTEGER NOT NULL,
     path VARCHAR(120) NOT NULL,
     updated_at BIGINT,
-    number_of_samples INTEGER,
+    number_of_samples INTEGER
 );
 
 CREATE INDEX IF NOT EXISTS files_dataset_id ON files (dataset_id);
