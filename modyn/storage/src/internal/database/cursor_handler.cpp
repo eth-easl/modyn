767e4c2abfd04ac7517eaaadc239f02065e68434
Maximilian Böther <maxi+git@boether.de>
2023-11-06 08:45:59 +0100

    CI

--- modyn/storage/src/internal/database/cursor_handler.cpp
+++ modyn/storage/src/internal/database/cursor_handler.cpp
@@ -4,6 +4,8 @@
 #include <libpq/libpq-fs.h>
 #include <soci/soci.h>
 
+#include <limits>
+
 using namespace modyn::storage;
 
 std::vector<SampleRecord> CursorHandler::yield_per(const int64_t number_of_rows_to_fetch) {
@@ -13,6 +15,8 @@ std::vector<SampleRecord> CursorHandler::yield_per(const int64_t number_of_rows_
   switch (driver_) {
     case DatabaseDriver::POSTGRESQL: {
       const std::string fetch_query = fmt::format("FETCH {} FROM {}", number_of_rows_to_fetch, cursor_name_);
+      ASSERT(number_of_rows_to_fetch <= std::numeric_limits<int>::max,
+             "Postgres can only accept up to MAX_INT rows per iteration");
 
       PGresult* result = PQexec(postgresql_conn_, fetch_query.c_str());
 
@@ -22,17 +26,18 @@ std::vector<SampleRecord> CursorHandler::yield_per(const int64_t number_of_rows_
         return records;
       }
 
-      const uint64_t rows = static_cast<uint64_t>(PQntuples(result));
+      const auto rows = static_cast<uint64_t>(PQntuples(result));
       records.resize(rows);
 
       for (uint64_t i = 0; i < rows; ++i) {
         SampleRecord record{};
-        record.id = std::stoll(PQgetvalue(result, i, 0));
+        const auto row_idx = static_cast<int>(i);
+        record.id = std::stoll(PQgetvalue(result, row_idx, 0));
         if (number_of_columns_ > 1) {
-          record.column_1 = std::stoll(PQgetvalue(result, i, 1));
+          record.column_1 = std::stoll(PQgetvalue(result, row_idx, 1));
         }
         if (number_of_columns_ == 3) {
-          record.column_2 = std::stoll(PQgetvalue(result, i, 2));
+          record.column_2 = std::stoll(PQgetvalue(result, row_idx, 2));
         }
 
         records[i] = record;
@@ -44,6 +49,7 @@ std::vector<SampleRecord> CursorHandler::yield_per(const int64_t number_of_rows_
     }
     case DatabaseDriver::SQLITE3: {
       int64_t retrieved_rows = 0;
+      records.reserve(number_of_rows_to_fetch);
       for (auto& row : *rs_) {
         SampleRecord record{};
         record.id = StorageDatabaseConnection::get_from_row<int64_t>(row, 0);
@@ -53,8 +59,8 @@ std::vector<SampleRecord> CursorHandler::yield_per(const int64_t number_of_rows_
         if (number_of_columns_ == 3) {
           record.column_2 = StorageDatabaseConnection::get_from_row<int64_t>(row, 2);
         }
-        records[retrieved_rows] = record;
-        retrieved_rows++;
+        records.push_back(record);
+        ++retrieved_rows;
         if (retrieved_rows >= number_of_rows_to_fetch) {
           break;
         }
