80436003fdc08fdee9701cb13f37848fb0d1e01e
Maximilian Böther <maxi+git@boether.de>
2023-11-12 20:48:33 +0100

    register datasets from config

--- modyn/storage/src/storage_server.cpp
+++ modyn/storage/src/storage_server.cpp
@@ -12,9 +12,39 @@ using namespace modyn::storage;
 
 void StorageServer::run() {
   /* Run the storage service. */
-  SPDLOG_INFO("Running storage service.");
+  SPDLOG_INFO("Running storage service. Creating tables.");
 
   connection_.create_tables();
+  SPDLOG_INFO("Running storage service. Initializing datasets from config.");
+
+  for (const YAML::Node& dataset_node : config_["storage"]["datasets"]) {
+    const std::string dataset_id = dataset_node["name"].as<std::string>();
+    const std::string base_path = dataset_node["base_path"].as<std::string>();
+    const std::string filesystem_wrapper_type = dataset_node["filesystem_wrapper_type"].as<std::string>();
+    const std::string file_wrapper_type = dataset_node["file_wrapper_type"].as<std::string>();
+    const std::string description = dataset_node["description"].as<std::string>();
+    const std::string version = dataset_node["version"].as<std::string>();
+    const std::string file_wrapper_config = dataset_node["file_wrapper_config"].as<std::string>();
+
+    bool ignore_last_timestamp = false;
+    int file_watcher_interval = 5;
+
+    if (dataset_node["ignore_last_timestamp"]) {
+      ignore_last_timestamp = dataset_node["ignore_last_timestamp"].as<bool>();
+    }
+
+    if (dataset_node["file_watcher_interval"]) {
+      file_watcher_interval = dataset_node["file_watcher_interval"].as<int>();
+    }
+
+    const bool success = connection_.add_dataset(
+        dataset_id, base_path, FilesystemWrapper::get_filesystem_wrapper_type(filesystem_wrapper_type),
+        FileWrapper::get_file_wrapper_type(file_wrapper_type), description, version, file_wrapper_config,
+        ignore_last_timestamp, file_watcher_interval);
+    if (!success) {
+      SPDLOG_ERROR(fmt::format("Could not register dataset {} - potentially it already exists.", dataset_id));
+    }
+  }
 
   SPDLOG_INFO("Starting file watcher watchdog.");
 
