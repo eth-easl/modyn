d70077bd59ea8e6b05f6932b7215974aff051b12
Ties Robroek <titr@itu.dk>
2023-11-14 14:05:18 +0100

    tidy

--- modyn/common/trigger_sample/src/trigger_sample_storage.cpp
+++ modyn/common/trigger_sample/src/trigger_sample_storage.cpp
@@ -254,9 +254,9 @@ std::vector<std::string> get_matching_files(const char* folder, const char* patt
  * @return int64_t Data size
  */
 int64_t read_data_size_from_header(std::ifstream& file) {
-  std::array<char, 2> header_chars;
+  std::array<char, 2> header_chars = {};
   file.read(header_chars.data(), 2);
-  uint64_t header_length = header_chars[1];
+  uint64_t header_length = static_cast<unsigned char>(header_chars[1]);
   header_length <<= 8u;
   header_length += header_chars[0];
 
