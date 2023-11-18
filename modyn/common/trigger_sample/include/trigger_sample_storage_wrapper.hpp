3d17e7511c67d6adec277111dc69c8c56228e082
Titus Robroek <ties@trobroek.com>
2023-11-18 09:23:29 +0100

    Move `TriggerSampleStorage` to a C++ extension (#326)
    Loading and saving trigger samples is a bottleneck that needs to be
    resolved. This C++ extension provides a considerable speedup to
    alleviate these issues.

---
+++ modyn/common/trigger_sample/include/trigger_sample_storage_wrapper.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstdint>
+
+extern "C" {
+
+int64_t get_num_samples_in_file(const char* filename);
+
+void* get_worker_samples(const char* folder, int64_t* size, const char* pattern, int64_t start_index,
+                         int64_t worker_subset_size);
+void* get_all_samples(const char* folder, int64_t* size, const char* pattern);
+
+void* parse_file(const char* filename, int64_t* size);
+
+void write_files(const char* filenames[], const void* data, int64_t data_lengths[], const char* headers[],
+                 int64_t header_length, std::uint64_t num_files);
+
+void release_data(void* data);
+}
