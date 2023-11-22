fd395735406a5579ef363a4e38b74866ab56ece4
Maximilian Böther <maxi+git@boether.de>
2023-11-22 16:29:39 +0100

    fix wrong f1 implementation

--- modyn/evaluator/internal/metrics/f1_score.py
+++ modyn/evaluator/internal/metrics/f1_score.py
@@ -86,11 +86,9 @@ class F1Score(AbstractDecomposableMetric):
         false_negatives = self.classification_matrix[2]
 
         denominator = 2 * true_positives + false_positives + false_negatives
-        if not np.all(denominator):
-            self.warning("Observed denominator for F1-scores to be zero.")
-            return 0
-
-        f1_scores = 2 * true_positives / denominator
+        numerator = 2 * true_positives
+        # For whichever class the denominator is zero, we output a F1 score for this class of zero
+        f1_scores = np.divide(numerator, denominator, out=np.zeros(numerator.shape, dtype=float), where=denominator!=0)
 
         if self.average == F1ScoreTypes.BINARY:
             return f1_scores[self.pos_label]
