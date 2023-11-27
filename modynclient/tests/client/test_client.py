3e7d182c271bc339edb932bee22dcda6421b7000
Jingyi Zhu <jinzhu@student.ethz.ch>
2023-11-28 00:02:44 +0100

    Add modynclient unittests. TODO: Add modynclient grpc_handler unittest. Add modynclient integration tests

---
+++ modynclient/tests/client/test_client.py
@@ -0,0 +1,120 @@
+import os
+import pathlib
+import shutil
+from typing import Optional
+from unittest.mock import MagicMock, patch
+
+from modynclient.client import Client
+from modynclient.client.internal.grpc_handler import GRPCHandler
+
+EVALUATION_DIRECTORY: pathlib.Path = pathlib.Path(os.path.realpath(__file__)).parent / "test_eval_dir"
+PIPELINE_ID = 42
+
+
+def get_minimal_pipeline_config() -> dict:
+    return {
+        "pipeline": {"name": "Test"},
+        "model": {"id": "ResNet18"},
+        "model_storage": {"full_model_strategy": {"name": "PyTorchFullModel"}},
+        "training": {
+            "gpus": 1,
+            "device": "cpu",
+            "dataloader_workers": 1,
+            "use_previous_model": True,
+            "initial_model": "random",
+            "initial_pass": {"activated": False},
+            "learning_rate": 0.1,
+            "batch_size": 42,
+            "optimizers": [
+                {"name": "default1", "algorithm": "SGD", "source": "PyTorch", "param_groups": [{"module": "model"}]},
+            ],
+            "optimization_criterion": {"name": "CrossEntropyLoss"},
+            "checkpointing": {"activated": False},
+            "selection_strategy": {"name": "NewDataStrategy", "maximum_keys_in_memory": 10},
+        },
+        "data": {"dataset_id": "test", "bytes_parser_function": "def bytes_parser_function(x):\n\treturn x"},
+        "trigger": {"id": "DataAmountTrigger", "trigger_config": {"data_points_for_trigger": 1}},
+    }
+
+
+def get_minimal_evaluation_config() -> dict:
+    return {
+        "device": "cpu",
+        "datasets": [
+            {
+                "dataset_id": "MNIST_eval",
+                "bytes_parser_function": "def bytes_parser_function(data: bytes) -> bytes:\n\treturn data",
+                "dataloader_workers": 2,
+                "batch_size": 64,
+                "metrics": [{"name": "Accuracy"}],
+            }
+        ],
+    }
+
+
+def get_minimal_system_config() -> dict:
+    return {}
+
+
+def noop_constructor_mock(
+    self,
+    client_config: dict,
+    pipeline_config: dict,
+    eval_directory: pathlib.Path,
+    start_replay_at: Optional[int] = None,
+    stop_replay_at: Optional[int] = None,
+    maximum_triggers: Optional[int] = None,
+) -> None:
+    pass
+
+
+def sleep_mock(duration: int):
+    raise KeyboardInterrupt
+
+
+def setup():
+    if EVALUATION_DIRECTORY.is_dir():
+        shutil.rmtree(EVALUATION_DIRECTORY)
+    EVALUATION_DIRECTORY.mkdir(0o777)
+
+
+def teardown():
+    shutil.rmtree(EVALUATION_DIRECTORY)
+
+
+@patch.object(GRPCHandler, "init_supervisor", return_value=None)
+@patch("modyn.utils.grpc_connection_established", return_value=True)
+def get_non_connecting_client(
+    test_connection_established,
+    test_init_supervisor,
+) -> Client:
+    client = Client(get_minimal_system_config(), get_minimal_pipeline_config(), EVALUATION_DIRECTORY)
+    return client
+
+
+def test_initialization() -> None:
+    get_non_connecting_client()  # pylint: disable=no-value-for-parameter
+
+
+@patch.object(GRPCHandler, "start_pipeline", return_value={"pipeline_id": PIPELINE_ID})
+def test_start_pipeline(test_start_pipeline: MagicMock):
+    client = get_non_connecting_client()  # pylint: disable=no-value-for-parameter
+    started = client.start_pipeline()
+
+    test_start_pipeline.assert_called_once_with(
+        get_minimal_pipeline_config(), EVALUATION_DIRECTORY, None, None, None
+    )
+    assert started is True
+    assert client.pipeline_id == PIPELINE_ID
+
+
+@patch.object(GRPCHandler, "start_pipeline", return_value={"pipeline_id": -1, "exception": "an error"})
+def test_start_pipeline_failed(test_start_pipeline: MagicMock):
+    client = get_non_connecting_client()  # pylint: disable=no-value-for-parameter
+    started = client.start_pipeline()
+
+    test_start_pipeline.assert_called_once_with(
+        get_minimal_pipeline_config(), EVALUATION_DIRECTORY, None, None, None
+    )
+    assert started is False
+    assert client.pipeline_id is None
---
+++ modynclient/tests/client/test_entrypoint.py
@@ -0,0 +1,67 @@
+# pylint: disable=unused-argument
+""" This tests that the entry point script for the supervisor
+successfully runs through. This is _not_ the place for an integration test.
+"""
+import os
+import pathlib
+import tempfile
+from typing import Optional
+from unittest.mock import MagicMock, patch
+
+from modynclient.client import Client
+
+SCRIPT_PATH = pathlib.Path(os.path.realpath(__file__))
+
+EXAMPLE_PIPELINE = SCRIPT_PATH.parent.parent.parent / "config" / "examples" / "mnist.yaml"
+EXAMPLE_SYSTEM_CONFIG = SCRIPT_PATH.parent.parent.parent / "config" / "examples" / "modyn_client_config.yaml"
+
+NO_FILE = SCRIPT_PATH.parent / "thisshouldnot.exist"
+
+
+def noop_constructor_mock(
+    self,
+    client_config: dict,
+    pipeline_config: dict,
+    eval_directory: pathlib.Path,
+    start_replay_at: Optional[int] = None,
+    stop_replay_at: Optional[int] = None,
+    maximum_triggers: Optional[int] = None,
+) -> None:
+    pass
+
+
+def noop_poll_pipeline_status(self) -> None:
+    pass
+
+
+@patch.object(Client, "__init__", noop_constructor_mock)
+@patch.object(Client, "start_pipeline", return_value=True)
+@patch.object(Client, "poll_pipeline_status", noop_poll_pipeline_status)
+def test_modyn_client_script_runs(script_runner):
+    with tempfile.TemporaryDirectory() as eval_dir:
+        ret = script_runner.run("_modyn_client", str(EXAMPLE_PIPELINE), str(EXAMPLE_SYSTEM_CONFIG), str(eval_dir))
+        assert ret.success
+
+
+@patch.object(Client, "__init__", noop_constructor_mock)
+def test_supervisor_script_fails_on_non_existing_system_config(script_runner):
+    with tempfile.TemporaryDirectory() as eval_dir:
+        assert not NO_FILE.is_file(), "File that shouldn't exist exists."
+        ret = script_runner.run("_modyn_client", str(EXAMPLE_PIPELINE), str(NO_FILE), str(eval_dir))
+        assert not ret.success
+
+
+@patch.object(Client, "__init__", noop_constructor_mock)
+def test_supervisor_script_fails_on_non_existing_pipeline_config(script_runner):
+    with tempfile.TemporaryDirectory() as eval_dir:
+        assert not NO_FILE.is_file(), "File that shouldn't exist exists."
+        ret = script_runner.run("_modyn_client", str(NO_FILE), str(EXAMPLE_SYSTEM_CONFIG), str(eval_dir))
+        assert not ret.success
+
+
+@patch.object(Client, "__init__", noop_constructor_mock)
+def test_supervisor_script_fails_on_non_existing_eval_dir(script_runner):
+    eval_dir = pathlib.Path("unknownfolder")
+    assert not eval_dir.is_dir(), "Directory that shouldn't exist exists."
+    ret = script_runner.run("_modyn_client", str(NO_FILE), str(EXAMPLE_SYSTEM_CONFIG), str(eval_dir))
+    assert not ret.success
