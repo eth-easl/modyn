433551d01a5859e27b2563719036ba4b2f217ea7
Maximilian Böther <maxi+git@boether.de>
2023-12-01 15:14:03 +0100

    moms spaghetti

--- modyn/supervisor/supervisor.py
+++ modyn/supervisor/supervisor.py
@@ -632,6 +632,41 @@ class Supervisor:
 
                     logger.info("At least evaluation finished, continuing.")
 
+        # all copy this deadline induced copy pasta is horrible and needs to be cleaned up with a separate eval matrix PR
+        while len(list(running_evals)):
+            one_eval_done = False
+            while not one_eval_done:
+                sleep(5)
+                for eval_id, tracker in list(running_evals): # iterate over copy to modify on the fly
+                    eval_running, eval_exception = self.grpc.is_evaluation_running(eval_id)
+                    done_trigger_id = eval_id_to_trigger[eval_id]
+                    done_model_id = eval_id_to_model[eval_id]
+
+                    if eval_exception:
+                        logger.info("Exception for evaluation {eval_id}, restarting")
+                        logger.info(f"Evaluating model {model} on trigger {trigger} for matrix (AGAIN)")
+
+                        device = self.matrix_gpus[device_idx]
+                        device_idx = (device_idx + 1) % len(self.matrix_gpus)
+
+                        running_evals = [(eid, tracker) for (eid, tracker) in running_evals if eid != eval_id] # remove from running evals
+                        evaluations = self.grpc.start_evaluation(done_model_id, self.pipeline_config, pipeline, done_trigger_id, device)
+                        assert len(evaluations) == 1
+                        eval_id = next(iter(evaluations))
+                        running_evals.append((eval_id, evaluations[eval_id]))
+                        eval_id_to_trigger[eval_id] = trigger
+                        eval_id_to_model[eval_id] = model
+
+                        continue
+
+                    if not eval_running:
+                        logger.info(f"Evaluation {eval_id} on trigger {done_trigger_id} and model {done_model_id} done.")
+                        one_eval_done = True
+                        running_evals = [(eid, tracker) for (eid, tracker) in running_evals if eid != eval_id]
+                        eval_result_writer: LogResultWriter = self._init_evaluation_writer("log", trigger)
+                        self.grpc.store_evaluation_results([eval_result_writer], {eval_id: tracker})
+                        self.pipeline_log["evaluation_matrix"][done_model_id][done_trigger_id] = eval_result_writer.results
+                        self._persist_pipeline_log()
 
                 
     def pipeline(self) -> None:
