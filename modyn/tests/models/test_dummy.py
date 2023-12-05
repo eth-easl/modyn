33757df7701f4c0f29cc4983ab3a5e75b6ceb004
Jingyi Zhu <jinzhu@student.ethz.ch>
2023-12-05 18:36:58 +0100

    fix dummy model unit test

--- modyn/tests/models/test_dummy.py
+++ modyn/tests/models/test_dummy.py
@@ -4,7 +4,7 @@ from modyn.models import Dummy
 
 def test_empty_forward():
     net = Dummy({"num_classes": 10}, "cpu", False)
-    input_data = torch.rand(30, 3, 32, 32)
+    input_data = torch.rand(10, 1, 2)
     output = net.model(input_data)
-    assert output.shape == (30, 3, 32, 32)
-    assert torch.equal(output, input_data)
+    assert output.shape == (10, 1, 2)
+    assert torch.equal(output, net.model.output(input_data))
