42aea5fb0965603e33f632293e917944ca6cbe1f
Jingyi Zhu <jinzhu@student.ethz.ch>
2024-04-09 00:25:05 +0200

    Fix set_file_timestamp datetime creation

--- exp_infra/dataset_utils/set_file_timestamp.py
+++ exp_infra/dataset_utils/set_file_timestamp.py
@@ -1,25 +1,27 @@
 import os
 import pathlib
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
-def create_timestamp(year: int, month: int = 1, day: int = 1) -> int:
-    return int(datetime(year=year, month=month, day=day, tzinfo=timezone.utc).timestamp())
+def create_timestamp(delta_day: int) -> int:
+    start_date=datetime(year=1970, month=1, day=1, tzinfo=timezone.utc)
+    new_date = start_date + timedelta(days=delta_day)
+    return int(new_date.timestamp())
 
-def set_one_file(f: pathlib.Path, year: int, month: int, day: int) -> int:
-    t = create_timestamp(year, month, day)
+def set_one_file(f: pathlib.Path, idx: int) -> int:
+    t = create_timestamp(idx)
     os.utime(f, (t, t))
     return t
 
 
 if __name__ == '__main__':
     base_dir = pathlib.Path("/scratch/jinzhu/modyn/datasets")
-    datasets = ['huffpost', 'arxiv']
+    datasets = ['huffpost', 'arxiv', 'yearbook']
 
     for d in datasets:
         dataset_dir = base_dir / d
         train_dir = base_dir / f"{d}_train"
-        files = list(dataset_dir.iterdir())
+        files = list(train_dir.iterdir())
 
         for i, f in enumerate(files):
-            t = set_one_file(f, 1970, 1, i+1)
+            t = set_one_file(f, i)
             print(t, os.path.getmtime(f), f)
