7dd49403c8f0708d1561f6a0943ef96bb70eb8f3
Jingyi Zhu <jinzhu@student.ethz.ch>
2024-04-08 19:47:30 +0200

    Fix exp script output dir

--- exp_infra/run/huffpost.sh
+++ exp_infra/run/huffpost.sh
@@ -3,7 +3,7 @@
 PIPELINE_CONFIG_DIR=exp_infra/pipeline_configs/huffpost
 MODYN_CONFIG=modynclient/config/examples/modyn_client_config.yaml
 REMOTE_LOG_DIR=/tmp/eval
-LOCAL_LOG_DIR=~/exp_results
+LOCAL_LOG_DIR=~/modyn_exp_results/modyn_results/huffpost
 
 # ./modynclient/client/modyn-client --start-replay-at 0 $PIPELINE_CONFIG_DIR/huffpost_time.yaml $MODYN_CONFIG $REMOTE_LOG_DIR --log-directory $LOCAL_LOG_DIR
 # ./modynclient/client/modyn-client --start-replay-at 0 $PIPELINE_CONFIG_DIR/huffpost_amount.yaml $MODYN_CONFIG $REMOTE_LOG_DIR --log-directory $LOCAL_LOG_DIR
