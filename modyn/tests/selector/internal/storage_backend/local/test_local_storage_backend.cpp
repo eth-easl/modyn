b5c211619b310143ee9ccf29f45ffea363c5a0fe
Ties Robroek <titr@itu.dk>
2024-05-06 14:08:46 +0200

    use vectors

--- modyn/tests/selector/internal/storage_backend/local/test_local_storage_backend.cpp
+++ modyn/tests/selector/internal/storage_backend/local/test_local_storage_backend.cpp
@@ -30,10 +30,10 @@ TEST_F(LocalStorageBackendTest, TestWriteReadSingle) {
   // Hardcoded data to write
   const std::string file = tmp_dir_ + "/test.npy";
   const std::string data_write("0123456789012345678901234567890");
-  std::string data_read(32, '\0');
+  std::vector<char> data_read(32, nullptr);
 
   modyn::selector::local_storage_backend::write_file(file.c_str(), data_write.c_str(), 0u, 2u);
-  modyn::selector::local_storage_backend::parse_file(file.c_str(), static_cast<void*>(data_read.c_str()), 0u, 2u, 0u);
+  modyn::selector::local_storage_backend::parse_file(file.c_str(), data_read.c_str(), 0u, 2u, 0u);
 
   // Check whether read data equals written data
   ASSERT_STREQ(data_read.data(), data_write.data());
@@ -43,13 +43,13 @@ TEST_F(LocalStorageBackendTest, TestWriteReadAllMulti) {
   // Hardcoded data to write
   const char* files[] = {"test_a.npy", "test_b.npy"};  // NOLINT
   const std::string data_write("0123456789012345678901234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTU");
-  std::string data_read(80, '\0');
+  std::vector<char> data_read(80, nullptr);
   std::array<int64_t, 2> data_lengths = {3u, 2u};
   std::array<int64_t, 2> data_offsets = {0u, 0u};
 
   modyn::selector::local_storage_backend::write_files_impl(files, data_write.c_str(), data_lengths.data(), 2u);
-  modyn::selector::local_storage_backend::parse_files_impl(files, static_cast<void*>(data_read.c_str()),
-                                                           data_lengths.data(), data_offsets.data(), 2u);
+  modyn::selector::local_storage_backend::parse_files_impl(files, data_read.c_str(), data_lengths.data(),
+                                                           data_offsets.data(), 2u);
 
   // Check whether read data equals written data
   ASSERT_STREQ(data_read.data(), data_write.data());
