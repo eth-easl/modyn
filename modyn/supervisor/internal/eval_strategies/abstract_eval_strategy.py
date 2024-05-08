9b60d7198d2faaac0dbb6660d3e6c817253d9bd6
Xianzhe Ma <xianzma@gmail.com>
2024-05-08 11:11:35 +0200

    renovate aggregated eval strategy

--- modyn/supervisor/internal/eval_strategies/abstract_eval_strategy.py
+++ modyn/supervisor/internal/eval_strategies/abstract_eval_strategy.py
@@ -8,4 +8,11 @@ class AbstractEvalStrategy(ABC):
 
     @abstractmethod
     def get_eval_interval(self, first_timestamp: int, last_timestamp: int) -> Iterable[tuple[int, int]]:
+        """
+        This method should return an iterable of tuples, where each tuple represents a left inclusive, right exclusive
+        evaluation interval.
+        :param first_timestamp: the timestamp of the first sample in this trigger.
+        :param last_timestamp: the timestamp of the last sample in this trigger.
+        :return: an iterable of tuples where each tuple represents a left inclusive, right exclusive evaluation interval.
+        """
         raise NotImplementedError
--- modyn/supervisor/internal/eval_strategies/aggregated_eval_strategy.py
+++ modyn/supervisor/internal/eval_strategies/aggregated_eval_strategy.py
@@ -7,15 +7,22 @@ from modyn.utils import convert_timestr_to_seconds
 class AggregatedEvalStrategy(AbstractEvalStrategy):
     def __init__(self, eval_strategy_config: dict):
         super().__init__(eval_strategy_config)
-        self.granularity = convert_timestr_to_seconds(self.eval_strategy_config["granularity"])
-        assert self.granularity > 0, "granularity must be greater than 0"
+        self.offsets = eval_strategy_config["offsets"]
 
     def get_eval_interval(self, first_timestamp: int, last_timestamp: int) -> Iterable[tuple[int, int]]:
-        intervals = [
-            (0, first_timestamp),  # historical data
-            (first_timestamp, last_timestamp + 1),  # current data
-            (last_timestamp + 1, last_timestamp + 1 + self.granularity),  # future data
-        ]
-        if first_timestamp == 0:
-            intervals.pop(0)
-        yield from intervals
+        for offset in self.offsets:
+            if offset == "-inf":
+                yield 0, first_timestamp
+            elif offset == "inf":
+                # +1 because the left bound of the evaluation interval is inclusive
+                yield last_timestamp + 1, -1
+            else:
+                offset = convert_timestr_to_seconds(offset)
+                if offset < 0:
+                    yield min(first_timestamp + offset, 0), first_timestamp
+                elif offset > 0:
+                    yield last_timestamp + 1, last_timestamp + offset + 1
+                else:
+                    # offset == 0. +1 because the right bound of the evaluation interval is exclusive,
+                    # we want to include samples with `last_timestamp` as its timestamp in evaluation dataset
+                    yield first_timestamp, last_timestamp + 1
