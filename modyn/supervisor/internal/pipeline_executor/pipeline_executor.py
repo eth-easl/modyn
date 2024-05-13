1fb40421196482edbbb1333adcf8affa2c4e9671
Jingyi Zhu <jinzhu@student.ethz.ch>
2024-05-13 23:50:35 +0200

    Fix merge bugs

--- modyn/supervisor/internal/pipeline_executor/pipeline_executor.py
+++ modyn/supervisor/internal/pipeline_executor/pipeline_executor.py
@@ -179,22 +179,15 @@ class PipelineExecutor:
 
     def _handle_new_data_batch(self, batch: list[tuple[int, int, int]]) -> bool:
         self._sw.start("trigger_inform", overwrite=True)
-        triggering_indices: Generator[int, None, None] = self.trigger.inform(batch)
-        num_triggers = self._handle_triggers_within_batch(batch, triggering_indices)
-
-        logger.info(f"There are {num_triggers} triggers in this batch.")
-        self.pipeline_log["supervisor"]["num_triggers"] += num_triggers
+        triggering_indices = self.trigger.inform(batch)
+        num_triggers = len(triggering_indices)
+        self.pipeline_log["supervisor"]["num_triggers"] += len(triggering_indices)
         self.pipeline_log["supervisor"]["trigger_batch_times"].append(
             {"batch_size": len(batch), "time": self._sw.stop("trigger_inform"), "num_triggers": num_triggers}
         )
 
-        if num_triggers == 0:
-            self._sw.start("selector_inform", overwrite=True)
-            selector_log = self.grpc.inform_selector(self.pipeline_id, batch)
-            self.pipeline_log["supervisor"]["selector_informs"].append(
-                {"total_selector_time": self._sw.stop(), "selector_log": selector_log}
-            )
-
+        logger.info(f"There are {num_triggers} triggers in this batch.")
+        self._handle_triggers_within_batch(batch, triggering_indices)
         return num_triggers > 0
 
     def _run_training(self, trigger_id: int) -> None:
@@ -284,12 +277,8 @@ class PipelineExecutor:
         previous_trigger_idx = 0
         logger.info("Handling triggers within batch.")
         self._update_pipeline_stage_and_enqueue_msg(PipelineStage.HANDLE_TRIGGERS_WITHIN_BATCH, MsgType.GENERAL)
-        num_triggers_in_batch = 0
-        last_trigger_id: Optional[int] = None
 
-        for triggering_idx in triggering_indices:
-            num_triggers_in_batch += 1
-
+        for i, triggering_idx in enumerate(triggering_indices):
             self._update_pipeline_stage_and_enqueue_msg(PipelineStage.INFORM_SELECTOR_AND_TRIGGER, MsgType.GENERAL)
             triggering_data = batch[previous_trigger_idx : triggering_idx + 1]
             previous_trigger_idx = triggering_idx + 1
@@ -320,33 +309,11 @@ class PipelineExecutor:
                 )
             else:
                 logger.info(f"Skipping training on empty trigger {trigger_id}]")
+
             self.num_triggers = self.num_triggers + 1
             if self.maximum_triggers is not None and self.num_triggers >= self.maximum_triggers:
-                return num_triggers_in_batch
+                break
 
-        # If no other trigger is coming in this batch,
-        # we have to inform the Selector about the remaining data in this batch.
-
-        remaining_data = batch[previous_trigger_idx:]
-        logger.info(f"There are {len(remaining_data)} data points remaining after the trigger.")
-
-        if len(remaining_data) > 0 and last_trigger_id is not None:
-            # These data points will be included in the next trigger
-            # because we inform the Selector about them,
-            # just like other batches with no trigger at all are included.
-            self._update_pipeline_stage_and_enqueue_msg(
-                PipelineStage.INFORM_SELECTOR_REMAINING_DATA,
-                MsgType.ID,
-                id_submsg(IdType.TRIGGER, last_trigger_id),
-            )
-            self._sw.start("selector_inform", overwrite=True)
-            selector_log = self.grpc.inform_selector(self.pipeline_id, remaining_data)
-            self.pipeline_log["supervisor"]["selector_informs"].append(
-                {"total_selector_time": self._sw.stop(), "selector_log": selector_log}
-            )
-
-        self._persist_pipeline_log()
-        return num_triggers_in_batch
 
         # we have to inform the Selector about the remaining data in this batch.
         if len(triggering_indices) == 0:
