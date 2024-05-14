5e1e98494a5099c870ee3efe6e98b9a3d1b43de9
Jingyi Zhu <jinzhu@student.ethz.ch>
2024-05-14 09:11:20 +0200

    Fix mypy

--- modyn/supervisor/internal/triggers/datadrifttrigger.py
+++ modyn/supervisor/internal/triggers/datadrifttrigger.py
@@ -186,9 +186,9 @@ class DataDriftTrigger(Trigger):
         current_dataloader = prepare_trigger_dataloader_fixed_keys(
             self.previous_trigger_id + 1,
             self.dataloader_info,
-            current_keys,
+            current_keys,   # type: ignore
             sample_size=self.sample_size,
-        )  # type: ignore
+        )
 
         # Download previous model as embedding encoder
         # TODO(417) Support custom model as embedding encoder
