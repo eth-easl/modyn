77c3d6f5570323c264eee75b0250c4f9645e6bf2
Robin Holzinger <robin.holzinger@tum.de>
2024-05-27 17:48:36 +0200

    command variable mamba or micromamba

--- scripts/python_compliance.sh
+++ scripts/python_compliance.sh
@@ -4,36 +4,49 @@ PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )
 
 pushd $PARENT_DIR
 
+if command -v mamba &> /dev/null
+then
+    MAMBA_CMD="mamba"
+elif command -v micromamba &> /dev/null
+then
+    MAMBA_CMD="micromamba"
+else
+    echo "Neither mamba nor micromamba could be found. Please ensure one of them is available when running the script."
+    echo "Checkout README.md for setup instruction."
+    exit 1
+fi
+
+
 echo "Running auto-formatters"
 
-micromamba run -n modyn isort . > /dev/null
-micromamba run -n modyn autopep8 modyn integrationtests --recursive --in-place --pep8-passes 2000 > /dev/null
-micromamba run -n modyn black modyn integrationtests --verbose --config black.toml > /dev/null
+$MAMBA_CMD run -n modyn isort . > /dev/null
+$MAMBA_CMD run -n modyn autopep8 modyn integrationtests --recursive --in-place --pep8-passes 2000 > /dev/null
+$MAMBA_CMD run -n modyn black modyn integrationtests --verbose --config black.toml > /dev/null
 
 echo "Running linters"
 
-if micromamba run -n modyn flake8 modyn ; then
+if $MAMBA_CMD run -n modyn flake8 modyn ; then
     echo "No flake8 errors"
 else
     echo "flake8 errors"
     exit 1
 fi
 
-if micromamba run -n modyn isort . --check --diff ; then
+if $MAMBA_CMD run -n modyn isort . --check --diff ; then
     echo "No isort errors"
 else
     echo "isort errors"
     exit 1
 fi
 
-if micromamba run -n modyn black --check modyn --config black.toml ; then
+if $MAMBA_CMD run -n modyn black --check modyn --config black.toml ; then
     echo "No black errors"
 else
     echo "black errors"
     exit 1
 fi
 
-if micromamba run -n modyn pylint modyn ; then
+if $MAMBA_CMD run -n modyn pylint modyn ; then
     echo "No pylint errors"
 else
     echo "pylint errors"
@@ -42,14 +55,14 @@ fi
 
 echo "Running tests"
 
-if micromamba run -n modyn pytest ; then
+if $MAMBA_CMD run -n modyn pytest ; then
     echo "No pytest errors"
 else
     echo "pytest errors"
     exit 1
 fi
 
-if micromamba run -n modyn mypy modyn ; then
+if $MAMBA_CMD run -n modyn mypy modyn ; then
     echo "No mypy errors"
 else
     echo "mypy errors"
