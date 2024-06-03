e9b049b207c66a32057d2bdef1ec9cb80a821f88
Xianzhe Ma <xianzma@gmail.com>
2024-06-03 20:31:12 +0200

    RHO-LOSS Step 1: fetch or create rho pipeline id (#457)
    As the title, this PR implements the logic to fetch or create rho
    pipeline id.

--- integrationtests/metadata_processor/integrationtest_metadata_processor.py
+++ integrationtests/metadata_processor/integrationtest_metadata_processor.py
@@ -47,7 +47,7 @@ def get_grpc_channel(config: dict, component: str) -> grpc.Channel:
 def send_metadata_and_check_database(processor_client: MetadataProcessorClient, config: dict) -> int:
     with MetadataDatabaseConnection(config) as database:
         pipeline_id = database.register_pipeline(
-            2, "ResNet18", "{}", False, ModelStorageStrategyConfig("PyTorchFullModel")
+            2, "ResNet18", "{}", False, "{}", "{}", ModelStorageStrategyConfig("PyTorchFullModel")
         )
 
     req = TrainingMetadataRequest(
