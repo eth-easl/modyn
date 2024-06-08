3334960ab5093110d61b7617d175027ba60f8691
Robin Holzinger <robin.holzinger@tum.de>
2024-06-09 00:46:32 +0200

    Add config models for more eval strategies

--- modyn/config/schema/pipeline/evaluation/strategy/__init__.py
+++ modyn/config/schema/pipeline/evaluation/strategy/__init__.py
@@ -2,15 +2,24 @@ from typing import Annotated, Union
 
 from pydantic import Field
 
+from .between_two_triggers import *  # noqa
+from .between_two_triggers import BetweenTwoTriggersEvalStrategyConfig
 from .offset import *  # noqa
 from .offset import OffsetEvalStrategyConfig
+from .periodic import *  # noqa
+from .periodic import PeriodicEvalStrategyConfig
 from .slicing import *  # noqa
 from .slicing import SlicingEvalStrategyConfig
+from .static import *  # noqa
+from .static import StaticEvalTriggerConfig
 
 EvalStrategyConfig = Annotated[
     Union[
         SlicingEvalStrategyConfig,
         OffsetEvalStrategyConfig,
+        BetweenTwoTriggersEvalStrategyConfig,
+        PeriodicEvalStrategyConfig,
+        StaticEvalTriggerConfig,
     ],
     Field(discriminator="type"),
 ]
---
+++ modyn/const/types.py
@@ -0,0 +1,3 @@
+from typing import Literal
+
+TimeUnit = Literal["s", "m", "h", "d", "w", "y"]
