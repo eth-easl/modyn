336551ef95717d291b54ad47036dc0cb7bbf63b0
Maximilian Böther <maxi+git@boether.de>
2024-06-14 15:34:11 +0200

    readd rs2

--- modyn/trainer_server/internal/trainer/remote_downsamplers/__init__.py
+++ modyn/trainer_server/internal/trainer/remote_downsamplers/__init__.py
@@ -7,6 +7,8 @@ from .remote_kcenter_greedy_downsampling_strategy import RemoteKcenterGreedyDown
 from .remote_loss_downsampling import RemoteLossDownsampling  # noqa: F401
 from .remote_submodular_downsampling_strategy import RemoteSubmodularDownsamplingStrategy  # noqa: F401
 from .remote_uncertainty_downsampling_strategy import RemoteUncertaintyDownsamplingStrategy  # noqa: F401
+from .remote_rs2_downsampling import RemoteRS2Downsampling  # noqa: F401
+
 
 files = os.listdir(os.path.dirname(__file__))
 files.remove("__init__.py")
