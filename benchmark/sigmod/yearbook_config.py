a73314a5ab6b68cb109e087147c1599444ae7b9a
Maximilian Böther <maxi+git@boether.de>
2024-06-18 14:25:39 +0200

    antoher yearbook fix

--- benchmark/sigmod/yearbook_config.py
+++ benchmark/sigmod/yearbook_config.py
@@ -132,7 +132,7 @@ def gen_yearbook_config(
                     models="matrix",
                     datasets=["yearbook-test"],
                     strategy=PeriodicEvalStrategyConfig(
-                        every="1d", interval="[-25h; +25h]", start_timestamp=0, end_timestamp=1400000
+                        every="1d", interval="[-25h; +25h]", start_timestamp=0, end_timestamp=7258000
                     ),
                 ),
             ],
