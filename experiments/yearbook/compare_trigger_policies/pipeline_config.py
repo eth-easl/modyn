7d694d0d4e8de286b0dd1ec255c434efd3fff599
Robin Holzinger <robin.holzinger@tum.de>
2024-06-24 15:14:07 +0200

    feat: Adjust `yearbook` multi-pipeline experiment  (#498)

--- experiments/yearbook/compare_trigger_policies/pipeline_config.py
+++ experiments/yearbook/compare_trigger_policies/pipeline_config.py
@@ -7,24 +7,27 @@ from modyn.config import (
     TrainingConfig,
 )
 from modyn.config.schema.pipeline.config import (
-    EvalDataConfig,
-    EvalStrategyModel,
     EvaluationConfig,
-    FullModelStrategy,
-    Metric,
     ModelConfig,
     ModynPipelineConfig,
-    NewDataStrategyConfig,
     Pipeline,
     PipelineModelStorageConfig,
     TriggerConfig,
 )
+from modyn.config.schema.pipeline.evaluation.config import EvalDataConfig
+from modyn.config.schema.pipeline.evaluation.handler import EvalHandlerConfig
+from modyn.config.schema.pipeline.evaluation.metrics import AccuracyMetricConfig, F1ScoreMetricConfig
+from modyn.config.schema.pipeline.model_storage import FullModelStrategy
+from modyn.config.schema.pipeline.sampling.config import NewDataStrategyConfig
 
 
-def gen_pipeline_config(name: str, trigger: TriggerConfig, eval_strategy: EvalStrategyModel) -> ModynPipelineConfig:
+def gen_pipeline_config(
+    name: str, trigger: TriggerConfig, eval_handlers: list[EvalHandlerConfig]
+) -> ModynPipelineConfig:
+    num_classes = 2
     return ModynPipelineConfig(
         pipeline=Pipeline(name=name, description="Yearbook pipeline for comparing trigger policies", version="0.0.1"),
-        model=ModelConfig(id="YearbookNet", config={"num_input_channels": 3, "num_classes": 2}),
+        model=ModelConfig(id="YearbookNet", config={"num_input_channels": 3, "num_classes": num_classes}),
         model_storage=PipelineModelStorageConfig(full_model_strategy=FullModelStrategy(name="PyTorchFullModel")),
         training=TrainingConfig(
             gpus=1,
@@ -33,6 +36,7 @@ def gen_pipeline_config(name: str, trigger: TriggerConfig, eval_strategy: EvalSt
             use_previous_model=True,
             initial_model="random",
             batch_size=64,
+            shuffle=True,
             optimizers=[
                 OptimizerConfig(
                     name="default",
@@ -43,49 +47,58 @@ def gen_pipeline_config(name: str, trigger: TriggerConfig, eval_strategy: EvalSt
             ],
             optimization_criterion=OptimizationCriterion(name="CrossEntropyLoss"),
             checkpointing=CheckpointingConfig(activated=False),
-            selection_strategy=NewDataStrategyConfig(
-                maximum_keys_in_memory=1000, storage_backend="database", limit=-1, tail_triggers=0
-            ),
+        ),
+        selection_strategy=NewDataStrategyConfig(
+            maximum_keys_in_memory=1000, storage_backend="database", limit=-1, tail_triggers=0
         ),
         data=DataConfig(
             dataset_id="yearbook",
             transformations=[],
-            bytes_parser_function="""
-            import warnings
-            import torch
-            def bytes_parser_function(data: memoryview) -> torch.Tensor:
-                with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=UserWarning)
-                return torch.frombuffer(data, dtype=torch.float32).reshape(3, 32, 32)
-            """,
+            bytes_parser_function=(
+                "import warnings\n"
+                "import torch\n"
+                "def bytes_parser_function(data: memoryview) -> torch.Tensor:\n"
+                "    with warnings.catch_warnings():\n"
+                "        warnings.simplefilter('ignore', category=UserWarning)\n"
+                "        return torch.frombuffer(data, dtype=torch.float32).reshape(3, 32, 32)"
+            ),
         ),
         trigger=trigger,
         evaluation=EvaluationConfig(
-            eval_strategy=eval_strategy,
+            handlers=eval_handlers,
             device="cuda:0",
             result_writers=["json"],
             datasets=[
                 EvalDataConfig(
-                    dataset_id="yearbook",
-                    bytes_parser_function="""
-                        import torch
-                        import numpy as np
-                        def bytes_parser_function(data: bytes) -> torch.Tensor:
-                            return torch.from_numpy(np.frombuffer(data, dtype=np.float32)).reshape(3, 32, 32)
-                    """,
+                    dataset_id=yb_dataset_name,
+                    bytes_parser_function=(
+                        "import torch\n"
+                        "import numpy as np\n"
+                        "def bytes_parser_function(data: bytes) -> torch.Tensor:\n"
+                        "    return torch.from_numpy(np.frombuffer(data, dtype=np.float32)).reshape(3, 32, 32)\n"
+                    ),
                     batch_size=64,
                     dataloader_workers=2,
                     metrics=[
-                        Metric(
-                            name="Accuracy",
-                            evaluation_transformer_function="""
-                                import torch
-                                def evaluation_transformer_function(model_output: torch.Tensor) -> torch.Tensor:
-                                    return torch.argmax(model_output, dim=-1)
-                            """,
-                        )
+                        AccuracyMetricConfig(
+                            evaluation_transformer_function=(
+                                "import torch\n"
+                                "def evaluation_transformer_function(model_output: torch.Tensor) -> torch.Tensor:\n"
+                                "    return torch.argmax(model_output, dim=-1)\n"
+                            ),
+                        ),
+                        F1ScoreMetricConfig(
+                            evaluation_transformer_function=(
+                                "import torch\n"
+                                "def evaluation_transformer_function(model_output: torch.Tensor) -> torch.Tensor:\n"
+                                "   return torch.argmax(model_output, dim=-1)"
+                            ),
+                            num_classes=num_classes,
+                            average="weighted",
+                        ),
                     ],
                 )
+                for yb_dataset_name in ["yearbook", "yearbook_test"]
             ],
         ),
     )
