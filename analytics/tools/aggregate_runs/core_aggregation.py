59d8ee9e4f5758f5448bdc357cc1fd89985050be
Robin Holzinger <robin.holzinger@tum.de>
2024-06-24 22:00:23 +0200

    feat: Add unique sequence number for aggregation into supervisor (#548)

--- analytics/tools/aggregate_runs/core_aggregation.py
+++ analytics/tools/aggregate_runs/core_aggregation.py
@@ -3,7 +3,6 @@ from copy import deepcopy
 from pathlib import Path
 
 import pandas as pd
-
 from analytics.app.data.transform import dfs_models_and_evals, logs_dataframe
 from analytics.tools.aggregate_runs.dir_utils import load_multiple_logfiles
 from analytics.tools.aggregate_runs.pipeline_equivalence import assert_pipeline_equivalence
