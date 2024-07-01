cddc9ead7e6aa5b57bf7c3d0c731a63b79af24bb
Xianzhe Ma <xianzma@gmail.com>
2024-07-01 10:16:30 +0200

    yearbook

--- modyn/models/yearbooknet/yearbooknet.py
+++ modyn/models/yearbooknet/yearbooknet.py
@@ -35,8 +35,8 @@ class YearbookNetModel(CoresetSupportingModule):
             nn.Conv2d(in_channels, out_channels, 3, padding=1), nn.BatchNorm2d(out_channels), nn.ReLU(), nn.MaxPool2d(2)
         )
 
-    # pylint: disable-next=unused-argument
     def forward(self, data: torch.Tensor, sample_ids: Optional[list[int]] = None) -> torch.Tensor:
+        del sample_ids
         data = self.enc(data)
         data = torch.mean(data, dim=(2, 3))
         data = self.embedding_recorder(data)
