d159959d416193c0f6922e628e0900bad69d9070
Maximilian Böther <maxi+git@boether.de>
2024-07-04 00:05:19 +0200

    model dump

--- modyn/supervisor/internal/triggers/embedding_encoder_utils/embedding_encoder_downloader.py
+++ modyn/supervisor/internal/triggers/embedding_encoder_utils/embedding_encoder_downloader.py
@@ -29,7 +29,7 @@ class EmbeddingEncoderDownloader:
         base_dir: pathlib.Path,
         model_storage_address: str,
     ):
-        self.modyn_config = modyn_config.model_dump() # todo use actual model
+        self.modyn_config = modyn_config.model_dump(by_alias=True) # todo use actual model
         self.pipeline_id = pipeline_id
         self.base_dir = base_dir
         assert self.base_dir.exists(), f"Temporary Directory {self.base_dir} should have been created."
