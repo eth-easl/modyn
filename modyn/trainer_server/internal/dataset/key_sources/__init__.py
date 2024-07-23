b06c7acdcd08381c3e68aa4c6e19aa97c7f591c8
Xianzhe Ma <xianzma@gmail.com>
2024-07-23 22:15:43 +0200

    fix mypy

--- modyn/trainer_server/internal/dataset/key_sources/__init__.py
+++ modyn/trainer_server/internal/dataset/key_sources/__init__.py
@@ -9,8 +9,6 @@ from .abstract_key_source import AbstractKeySource  # noqa: F401
 from .local_key_source import LocalKeySource  # noqa: F401
 from .selector_key_source import SelectorKeySource  # noqa: F401
 
-KeySourceNames = Enum("KeySource", ["LOCAL_DOWNSAMPLER", "SELECTOR"])
-
 files = os.listdir(os.path.dirname(__file__))
 files.remove("__init__.py")
 __all__ = [f[:-3] for f in files if f.endswith(".py")]
