fc99fc6a0c03feea8dde7aaee4bd9b1dbe7d7a6f
Xianzhe Ma <xianzma@gmail.com>
2024-07-29 14:26:33 +0200

    do not clear storage table

--- scripts/volume_clearer.sh
+++ scripts/volume_clearer.sh
@@ -3,14 +3,14 @@ directory=/scratch/xianma/sigmod/modyntmp
 # map the local path to /tmp/hahaha in the docker container and change permission to 777
 docker run -it --rm  -v $directory:/tmp/hahaha postgres:15.2-alpine chmod -R 777 /tmp/hahaha
 
-rm -rf $directory/*
+#rm -rf $directory/*
 rm .modyn_configured
 docker volume rm $(docker volume ls -q)
 
-mkdir -m 777 $directory/local_storage
-mkdir -m 777 $directory/logs
-mkdir -m 777 $directory/metadatadb5
-mkdir -m 777 $directory/modelstorage
-mkdir -m 777 $directory/offline_dataset
-mkdir -m 777 $directory/storagedb2
-mkdir -m 777 $directory/trigger_samples
+rm -rf $directory/local_storage
+rm -rf $directory/logs
+rm -rf $directory/metadatadb5
+rm -rf $directory/modelstorage
+rm -rf $directory/offline_dataset
+#rm -rf $directory/storagedb2
+rm -rf $directory/trigger_samples
