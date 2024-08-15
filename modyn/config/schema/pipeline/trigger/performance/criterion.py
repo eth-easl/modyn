a0c790f63277c3bbb3e90709fa0998d78bb4f702
Robin Holzinger <robin.holzinger@tum.de>
2024-08-15 19:42:18 +0200

    Add config models for performance aware triggers

---
+++ modyn/config/schema/pipeline/trigger/performance/criterion.py
@@ -0,0 +1,136 @@
+from typing import Annotated, Literal
+
+from pydantic import Field
+
+from modyn.config.schema.base_model import ModynBaseModel
+
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                                 PerformanceCriterion                                                 #
+# -------------------------------------------------------------------------------------------------------------------- #
+# uses the evaluation results directory to derive a triggering decision
+
+
+class _NamedMetricPerformanceCriterion(ModynBaseModel):
+    metric: str = Field(
+        description="The metric that should be used for the comparison. Name as defined in the evaluation config."
+    )
+
+
+class StaticPerformanceThresholdCriterion(_NamedMetricPerformanceCriterion):
+    id: Literal["StaticPerformanceThresholdCriterion"] = Field("StaticPerformanceThresholdCriterion")
+    metric_threshold: float = Field(
+        0.0,
+        description=(
+            "The minimum target metric value that the model should achieve. If the performance is NOT reached "
+            "a trigger will be forced."
+        ),
+    )
+
+
+class DynamicPerformanceThresholdCriterion(_NamedMetricPerformanceCriterion):
+    """Triggers after comparison of current performance with the a rolling
+    average of historic performances after triggers."""
+
+    id: Literal["DynamicPerformanceThresholdCriterion"] = Field("DynamicPerformanceThresholdCriterion")
+    allowed_deviation: float = Field(
+        0.05,
+        description=(
+            "The allowed deviation from the expected performance. Will only trigger if the performance is "
+            "below the expected performance minus the allowed deviation."
+        ),
+    )
+
+
+# TODO: drift: rolling average + x % (instead of percentile)
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                           NumberMisclassificationCriterion                                           #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+
+class _NumberAvoidableMisclassificationCriterion(ModynBaseModel):
+    """Trigger based on the cumulated number of avoidable misclassifications.
+
+    An avoidable misclassification is a misclassification that would have been avoided if a trigger would have been.
+    E.g. if we currency see an accuracy of 80% but expect 95% with a trigger, 15% of the samples are avoidable
+    misclassifications.
+
+    We estimate the number of avoidable misclassifications with the measured and expected performance.
+
+    The cumulated number of misclassifications can be seen a regret of not triggering.
+
+    Advantage: cumulative metric allows to account for persisting slightly degraded performance. Eventually
+    the cumulated number of misclassifications will trigger a trigger.
+    """
+
+    expected_accuracy: float | None = Field(
+        None,
+        description=(
+            "The expected accuracy of the model. Used to estimate the number of avoidable misclassifications. "
+            "If not set, the expected performance will be inferred dynamically with a rolling average."
+        ),
+    )
+
+
+class StaticNumberAvoidableMisclassificationCriterion(_NumberAvoidableMisclassificationCriterion):
+    id: Literal["StaticNumberMisclassificationCriterion"] = Field("StaticNumberMisclassificationCriterion")
+    avoidable_misclassification_threshold: int = Field(
+        description="The threshold for the misclassification rate that will invoke a trigger."
+    )
+    allow_reduction: bool = Field(
+        False,
+        description=(
+            "If True, the cumulated number of misclassifications will be lowered through evaluations that are "
+            "better than the expected performance."
+        ),
+    )
+
+
+# class InferredNumberAvoidableMisclassificationCriterion(_NumberAvoidableMisclassificationCriterion):
+#     """
+#     `misclassification_threshold` will be inferred by the given inputs according to the following scheme:
+#     - `dataset_num_samples`: The number of samples in the dataset.
+#     - `dataset_time_range`: The time range of the dataset.
+#     - `target_num_triggers`: The number of triggers that are expected over the whole dataset assuming
+#         constant gradual drift.
+#     --> through the `expected_performance` we can infer the expected misclassification rate and thus the threshold
+#     """
+
+#     id: Literal["InferredNumberMisclassificationCriterion"] = Field(
+#         "InferredNumberMisclassificationCriterion"
+#     )
+
+#     dataset_num_samples: int = Field(
+#         description="The number of samples in the dataset."
+#     )
+#     dataset_time_range: int = Field(description="The time range of the dataset in epochs.")
+#     target_num_triggers: int = Field(
+#         description="The number of triggers that are expected over the whole dataset assuming gradual drift."
+#     )
+#     expected_performance: float = Field
+#     relative_avoidable_misclassification_threshold: float = Field(
+#         description=(
+#             "The threshold for the triggering misclassification"
+#             "The relative misclassification rate is the misclassification rate that would have been avoided "
+#             "if a trigger would have been invoked."
+#         )
+#     )
+
+#     @property
+#     def misclassification_threshold(self) -> float:
+#         epochs_per_trigger = self.dataset_time_range / self.target_num_triggers
+
+#         return self..metric_value
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
+#                                                         Union                                                        #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+PerformanceTriggerCriterion = Annotated[
+    StaticPerformanceThresholdCriterion
+    | DynamicPerformanceThresholdCriterion
+    | StaticNumberAvoidableMisclassificationCriterion,
+    Field(discriminator="id"),
+]
---
+++ modyn/config/schema/pipeline/trigger/performance/performance.py
@@ -0,0 +1,95 @@
+from typing import Literal
+
+from pydantic import Field, field_validator, model_validator
+
+from modyn.config.schema.base_model import ModynBaseModel
+from modyn.config.schema.pipeline.evaluation.config import EvalDataConfig
+from modyn.config.schema.pipeline.trigger.performance.criterion import (
+    PerformanceTriggerCriterion,
+)
+from modyn.const.types import ForecastingMethod, TriggerEvaluationMode
+
+
+class PerformanceTriggerEvaluationConfig(ModynBaseModel):
+    # max_samples_per_evaluation: int = Field(
+    #     1000,
+    #     description=(
+    #         "The maximum number of samples that should be used for the evaluation. If more are available, "
+    #         "downsampling will be used"
+    #     ),
+    # )  # TODO: currently not yet supported by the evaluator, we use a evaluation dataset that is sufficiently small
+
+    device: str = Field(description="The device the model should be put on.")
+    dataset: EvalDataConfig = Field(description="The dataset on which the model is evaluated.")
+    label_transformer_function: str | None = Field(
+        None, description="Function used to transform the label (tensors of integers)."
+    )
+
+    @field_validator("dataset")
+    @classmethod
+    def validate_metrics(cls, dataset: EvalDataConfig) -> EvalDataConfig:
+        """Assert that we have at least the accuracy metric."""
+        if not any(metric.name == "Accuracy" for metric in dataset.metrics):
+            raise ValueError("The accuracy metric is required for the performance trigger.")
+        return dataset
+
+
+class _InternalPerformanceTriggerConfig(ModynBaseModel):
+    detection_interval_data_points: int = Field(
+        description=(
+            "Specifies after how many samples another believe update (query density "
+            "estimation, accuracy evaluation) should be performed."
+        )
+    )
+    data_density_window_size: int = Field(
+        0,
+        description="The window size for the data density estimation. Only used for lookahead mode.",
+    )
+    performance_triggers_window_size: int = Field(
+        10,
+        description="The maximum number of evaluations after triggers to consider for computing the expect performance.",
+    )
+
+    evaluation: PerformanceTriggerEvaluationConfig = Field(
+        description="Configuration for the evaluation of the performance trigger."
+    )
+
+    decision_criteria: dict[str, PerformanceTriggerCriterion] = Field(
+        description=(
+            "The decision criteria to be used for the performance trigger. If any of the criteria is met, "
+            "the trigger will be executed. The criteria will be evaluated in the order they are defined. "
+            "Every criterion is linked to a metric. Some of the criteria implicitly only work on accuracy which is "
+            "the default metric that is always generated and cannot be disabled. To define a "
+            "`StaticPerformanceThresholdCriterion` on Accuracy, the evaluation config has to define the accuracy metric."
+            # TODO: maybe support custom aggregation functions
+        ),
+        min_length=1,
+    )
+
+    mode: TriggerEvaluationMode = Field(
+        "hindsight",
+        description="Whether to also consider forecasted future performance in the drift decision.",
+    )
+    forecasting_method: ForecastingMethod = Field(
+        "ridge_regression",
+        description=(
+            "The method to generate the forecasted performance and data density estimates. "
+            "Only used for lookahead mode."
+        ),
+    )
+
+    @model_validator(mode="after")
+    def validate_decision_criteria(self) -> "PerformanceTriggerConfig":
+        """Assert that all criteria use metrics that are defined in the
+        evaluation config."""
+        metrics = {metric.name for metric in self.evaluation.dataset.metrics}
+        for criterion in self.decision_criteria.values():
+            if criterion.metric not in metrics:
+                raise ValueError(
+                    f"Criterion {criterion.id} uses metric {criterion.metric} which is not defined in the evaluation config."
+                )
+        return self
+
+
+class PerformanceTriggerConfig(_InternalPerformanceTriggerConfig):
+    id: Literal["PerformanceTrigger"] = Field("PerformanceTrigger")
