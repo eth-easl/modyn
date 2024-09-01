844c6c696d0df08f9b5ef29832dbcd23b24d3e49
Robin Holzinger <robin.holzinger@tum.de>
2024-09-01 10:35:14 +0200

    Add hellinger distance

--- modyn/config/schema/pipeline/trigger/drift/evidently.py
+++ modyn/config/schema/pipeline/trigger/drift/evidently.py
@@ -5,39 +5,44 @@ from pydantic import Field
 from modyn.config.schema.pipeline.trigger.drift.metric import BaseMetric
 
 
-class _EvidentlyBaseDriftMetric(BaseMetric):
+class _EvidentlyPcaDriftMetric(BaseMetric):
     num_pca_component: int | None = Field(None)
 
 
-class EvidentlyModelDriftMetric(_EvidentlyBaseDriftMetric):
+class EvidentlyModelDriftMetric(_EvidentlyPcaDriftMetric):
     id: Literal["EvidentlyModelDriftMetric"] = Field("EvidentlyModelDriftMetric")
     bootstrap: bool = Field(False)
-    quantile_probability: float = 0.95
-    threshold: float = Field(0.55)
+    quantile_probability: float = Field(0.95, description="Threshold for the evidently api, unused in modyn. Usage discourged.")
+    threshold: float = Field(0.55, description="Threshold for the evidently api, unused in modyn. Usage discourged.")
 
 
-class EvidentlyRatioDriftMetric(_EvidentlyBaseDriftMetric):
+class EvidentlyRatioDriftMetric(_EvidentlyPcaDriftMetric):
     id: Literal["EvidentlyRatioDriftMetric"] = Field("EvidentlyRatioDriftMetric")
     component_stattest: str = Field(
         "wasserstein",
         description="The statistical test used to compare the components.",
     )
-    component_stattest_threshold: float = Field(0.1)
-    threshold: float = Field(0.2)
+    component_stattest_threshold: float = Field(0.1, description="Threshold for the evidently api, unused in modyn. Usage discourged.")
+    threshold: float = Field(0.2, description="Threshold for the evidently api, unused in modyn. Usage discourged.")
 
 
-class EvidentlySimpleDistanceDriftMetric(_EvidentlyBaseDriftMetric):
+class EvidentlySimpleDistanceDriftMetric(_EvidentlyPcaDriftMetric):
     id: Literal["EvidentlySimpleDistanceDriftMetric"] = Field("EvidentlySimpleDistanceDriftMetric")
     distance_metric: str = Field(
         "euclidean",
         description="The distance metric used for the distance calculation.",
     )
     bootstrap: bool = Field(False)
-    quantile_probability: float = 0.95
-    threshold: float = Field(0.2)
+    quantile_probability: float = Field(0.95, description="Threshold for the evidently api, unused in modyn. Usage discourged.")
+    threshold: float = Field(0.2, description="Threshold for the evidently api, unused in modyn. Usage discourged.")
+
+
+class EvidentlyHellingerDistanceDriftMetric(BaseMetric):
+    id: Literal["EvidentlyHellingerDistanceDriftMetric"] = Field("EvidentlyHellingerDistanceDriftMetric")
+    
 
 
 EvidentlyDriftMetric = Annotated[
-    EvidentlyModelDriftMetric | EvidentlyRatioDriftMetric | EvidentlySimpleDistanceDriftMetric,
+    EvidentlyModelDriftMetric | EvidentlyRatioDriftMetric | EvidentlySimpleDistanceDriftMetric | EvidentlyHellingerDistanceDriftMetric,
     Field(discriminator="id"),
 ]
