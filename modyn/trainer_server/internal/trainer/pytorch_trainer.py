f26374b26fb61d0746acaabdb490002bf6b87f7a
Xianzhe Ma <xianzma@gmail.com>
2024-09-12 00:31:28 +0800

    use no_grad

--- modyn/trainer_server/internal/trainer/pytorch_trainer.py
+++ modyn/trainer_server/internal/trainer/pytorch_trainer.py
@@ -1,6 +1,7 @@
 # pylint: disable=no-name-in-module
 from __future__ import annotations
 
+import contextlib
 import glob
 import io
 import itertools
@@ -560,7 +561,15 @@ class PytorchTrainer:
         self._downsampler.init_downsampler()
         self.start_embedding_recording_if_needed()
 
-        with torch.inference_mode(mode=not self._downsampler.requires_grad):
+        if self._downsampler.requires_grad:
+            context_manager = contextlib.nullcontext()
+        else:
+            context_manager = torch.no_grad()
+        # For model DLRM it does not work with inference mode with complaint later in the training phase
+        # that inference tensors cannot be saved for backward, so we have to use torch.no_grad() here.
+        # The guess for the reason is that some of the DLRM parameters are lazily created during the
+        # first forward pass and hence they are created as inference tensors if inference mode is used here.
+        with context_manager:
             big_batch_output = self._model.model(data) if self._downsampler.forward_required else torch.Tensor()
             embeddings = self.get_embeddings_if_recorded()
             self._downsampler.inform_samples(sample_ids, data, big_batch_output, target, embeddings)
@@ -904,7 +913,11 @@ class PytorchTrainer:
             sample_ids, target, data = self.preprocess_batch(batch)
             number_of_samples += len(sample_ids)
 
-            with torch.inference_mode(mode=not self._downsampler.requires_grad):
+            if self._downsampler.requires_grad:
+                context_manager = contextlib.nullcontext()
+            else:
+                context_manager = torch.no_grad()
+            with context_manager:
                 with torch.autocast(self._device_type, enabled=self._amp):
                     # compute the scores and accumulate them
                     model_output = self._model.model(data) if self._downsampler.forward_required else torch.Tensor()
