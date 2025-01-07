5ef97e2a8fd011302c9204b2570bcbe7661f82c7
Maximilian Böther <maxi+git@boether.de>
2025-01-07 15:17:44 +0100

    revert to fmt10; try clang 18 and 19

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,7 +8,7 @@ message(STATUS "Making fmt available.")
 FetchContent_Declare(
   fmt
   GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-  GIT_TAG 11.0.2
+  GIT_TAG 10.2.1
 )
 FetchContent_MakeAvailable(fmt)
 
