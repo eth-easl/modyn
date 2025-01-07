2f39286011459fbe5b7b4438420f6e428ada633e
Maximilian Böther <maxi+git@boether.de>
2025-01-07 14:26:37 +0100

    downgrade to compatible fmt version

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,7 +8,7 @@ message(STATUS "Making fmt available.")
 FetchContent_Declare(
   fmt
   GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-  GIT_TAG 11.1.1
+  GIT_TAG 11.0.2
 )
 FetchContent_MakeAvailable(fmt)
 
